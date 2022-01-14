edb9eef4e42e7c8859e6030efa9b9461c9adc37f
Gilga <pyua87@gmail.com>
2022-01-14 08:23:37 +0300

    enable

--- lua/global.lua
+++ lua/global.lua
@@ -1,5 +1,8 @@
 local g = vim.g
--- enable autostart for coq
--- g.coq_settings = {
---   auto_start = true,
--- }
+
+-- enable autostart for co-- dashboard-nvim
+g.dashboard_default_executive = "telescope"
+
+g.bufferline = {
+  add_in_buffer_number_order = true
+}
