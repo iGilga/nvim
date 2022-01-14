4e5148335c87f35c6f621e856b655f898e13bc9c
Gilga <pyua87@gmail.com>
2022-01-14 08:55:38 +0300

    add

---
+++ lua/plugins/todocomments.lua
@@ -0,0 +1,11 @@
+local setup = {
+  keywords = {
+    FIX = {
+      icon = '',
+      color = 'error',
+      alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE', 'fix', 'fixme', 'bug' },
+    },
+  },
+}
+
+require('todo-comments').setup()
