a0b0d307422031e78e532dc80b3c9a75ea4e3cca
Gilga <pyua87@gmail.com>
2022-01-14 08:27:06 +0300

    reorganize

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -21,16 +21,32 @@ nnoremap({ '<leader>s', ':w<cr>' })
 
 -- barbar.nvim
 local opts = { silent = true }
-nnoremap({ '<A-;>', ':BufferNext<CR>', opts })
-nnoremap({ '<A-l>', ':BufferPrevious<CR>', opts })
+nnoremap({ '<A-;>', ':BufferNext<cr>', opts })
+nnoremap({ '<A-l>', ':BufferPrevious<cr>', opts })
+nnoremap({ '<A-1>', ':BufferGoto 1<cr>', opts })
+nnoremap({ '<A-2>', ':BufferGoto 2<cr>', opts })
+nnoremap({ '<A-3>', ':BufferGoto 3<cr>', opts })
+nnoremap({ '<A-4>', ':BufferGoto 4<cr>', opts })
+nnoremap({ '<A-5>', ':BufferGoto 5<cr>', opts })
+nnoremap({ '<A-6>', ':BufferGoto 6<cr>', opts })
+nnoremap({ '<A-7>', ':BufferGoto 7<cr>', opts })
+nnoremap({ '<A-8>', ':BufferGoto 8<cr>', opts })
+nnoremap({ '<A-9>', ':BufferGoto 9<cr>', opts })
+nnoremap({ '<A-0>', ':BufferGotoLast<cr>', opts })
+nnoremap({ 'bs', ':BufferClose<cr>', opts })
+nnoremap({ 'bb', ':BufferPick<cr>', opts })
+nnoremap({ 'bn', ':BufferOrderByBufferNumber<cr>', opts })
+nnoremap({ 'bd', ':BufferOrderByDirectory<cr>', opts })
+nnoremap({ 'bl', ':BufferOrderByLanguage<cr>', opts })
 
 -- hop mapping
 nnoremap({ '<leader>w', "<cmd>lua require'hop'.hint_words()<cr>" })
 nnoremap({ '<leader>l', "<cmd>lua require'hop'.hint_lines()<cr>" })
 
 nnoremap({ '<leader>ss', ':SaveSession<cr>' })
--- nnoremap({ '<leader>sl', ':silent RestoreSession<cr>' })
--- nnoremap({ '<leader>f', '<cmd>lua vim.lsp.buf.formatting_sync()<cr>' })
+nnoremap({ '<leader>sl', ':silent Telescope session-lens search_session' })
 
 tnoremap({ '<c-t>', '<cmd>:NnnPicker<cr>' })
 nnoremap({ '<c-t>', '<cmd>:NnnPicker<cr>' })
+
+nnoremap({ '<leader>ga', "<cmd>lua require('utils.codeaction').code_actions()<cr>" })
