93f351f59b2d77811dfd6eefaacc654201a94069
Gilga <pyua87@gmail.com>
2022-01-18 12:03:59 +0300

    reorganized

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,9 +3,18 @@ local serversRange = { 'html', 'cssls', 'denols', 'vuels', 'bashls', 'yamlls', '
 local lspconfig = require('lspconfig')
 
 local on_attach = function(_, bufnr)
-  local function buf_ser_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  local function merge(...)
+    return vim.tbl_deep_extend('force', ...)
   end
+
+  local function buf_set_keymap(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+      options = merge(options, opts)
+    end
+    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, options)
+  end
+
   local function buf_set_option(...)
     vim.api.nvim_buf_set_option(bufnr, ...)
   end
@@ -16,50 +25,38 @@ local on_attach = function(_, bufnr)
   -- Mappings.
   local opts = { noremap = true }
 
-  function buf_map(bufnr, mode, lhs, rhs, opts)
-    local options = { noremap = true, silent = true }
-    if opts then
-      options = merge(options, opts)
-    end
-    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, options)
-  end
-
-  function merge(...)
-    return vim.tbl_deep_extend('force', ...)
-  end
-
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_map(bufnr, 'n', 'gd', '<cmd>lua require("telescope.builtin").lsp_definitions()<cr>')
-  buf_map(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-  buf_map(bufnr, 'n', 'gi', '<cmd>lua require("telescope.builtin").lsp_implementations()<cr>')
-  buf_map(bufnr, 'n', 'gt', '<cmd>lua require("telescope.builtin").lsp_type_definitions()<cr>')
-  buf_map(bufnr, 'n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references()<cr>')
+  buf_set_keymap('n', 'gd', '<cmd>lua require("telescope.builtin").lsp_definitions()<cr>')
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+  buf_set_keymap('n', 'gi', '<cmd>lua require("telescope.builtin").lsp_implementations()<cr>')
+  buf_set_keymap('n', 'gt', '<cmd>lua require("telescope.builtin").lsp_type_definitions()<cr>')
+  buf_set_keymap('n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references()<cr>')
   -- buf_map(bufnr, 'n', 'gn', '<cmd>lua require("cosmic-ui").rename()<cr>')
 
   -- diagnostics
-  buf_map(bufnr, 'n', '[g', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-  buf_map(bufnr, 'n', ']g', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-  buf_map(bufnr, 'n', 'ge', '<cmd>lua vim.diagnostic.open_float(nil, { scope = "line", })<cr>')
-  buf_map(bufnr, 'n', '<leader>ge', '<cmd>Telescope diagnostics bufnr=0<cr>')
+  buf_set_keymap('n', '[g', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+  buf_set_keymap('n', ']g', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+  buf_set_keymap('n', 'ge', '<cmd>lua vim.diagnostic.open_float(nil, { scope = "line", })<cr>')
+  buf_set_keymap('n', '<leader>ge', '<cmd>Telescope diagnostics bufnr=0<cr>')
 
   -- hover
-  buf_map(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
 
   -- code actions
   -- buf_map(bufnr, 'n', '<leader>ga', '<cmd>lua require("cosmic-ui").code_actions()<cr>')
   -- buf_map(bufnr, 'v', '<leader>ga', '<cmd>lua require("cosmic-ui").range_code_actions()<cr>')
 
   -- formatting
-  buf_map(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<cr>')
-  buf_map(bufnr, 'v', '<leader>gf', '<cmd>lua vim.lsp.buf.range_formatting()<cr>')
+  buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<cr>')
+  buf_set_keymap('v', '<leader>gf', '<cmd>lua vim.lsp.buf.range_formatting()<cr>')
 
   -- signature help
-  buf_map(bufnr, 'n', '<C-K>', '<cmd>lua require("lsp_signature").signature()<cr>')
+  buf_set_keymap('n', '<C-K>', '<cmd>lua require("lsp_signature").signature()<cr>')
 
   -- lsp workspace
-  buf_map(bufnr, 'n', '<leader>wd', '<cmd>Telescope diagnostics<cr>')
-  buf_map(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>')
-  buf_map(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>')
+  buf_set_keymap('n', '<leader>wd', '<cmd>Telescope diagnostics<cr>')
+  buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>')
+  buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>')
   -- buf_map(
   --   bufnr,
   --   'n',
