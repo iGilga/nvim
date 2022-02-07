a63a80c22e0903cb173e00f961821e08995e35a5
Gilga <pyua87@gmail.com>
2022-02-07 05:54:19 +0300

    Added support hop in telescope. Remove config vimgrep(now live_grep work)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,30 +5,38 @@ local actions = require('telescope.actions')
 
 local trouble = require('trouble.providers.telescope')
 
+P = function(v)
+  print(vim.inspect(v))
+  return v
+end
+
+if pcall(require, 'plenary') then
+  RELOAD = require('plenary.reload').reload_module
+
+  R = function(name)
+    RELOAD(name)
+    return require(name)
+  end
+end
 
 telescope.setup({
   defaults = {
     file_ignore_patterns = {
       '.git/',
     },
-    dynamic_preview_title = true,
-    vimgrep_arguments = {
-      'rg',
-      '--ignore',
-      '--hiden',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-      '--trim',
-    },
     mappings = {
       i = {
         ['<esc>'] = actions.close,
-        ['<C-h>'] = 'which_key',
+        ['<c-h>'] = 'which_key',
         ['<c-n>'] = trouble.open_with_trouble,
+        ['<c-l>'] = R('telescope').extensions.hop.hop, -- hop.hop_toggle_selection
+        ['<c-o>'] = function(prompt_bufnr)
+          local opts = {
+            callback = actions.toggle_selection,
+            loop_callback = actions.send_selected_to_qflist,
+          }
+          telescope.extensions.hop._hop_loop(prompt_bufnr, opts)
+        end,
       },
       n = {
         ['<c-n>'] = trouble.open_with_trouble,
@@ -43,6 +51,64 @@ telescope.setup({
       case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
       -- the default case_mode is "smart_case"
     },
+    hop = {
+      -- the shown `keys` are the defaults, no need to set `keys` if defaults work for you ;)
+      keys = {
+        'a',
+        's',
+        'd',
+        'f',
+        'g',
+        'h',
+        'j',
+        'k',
+        'l',
+        ';',
+        'q',
+        'w',
+        'e',
+        'r',
+        't',
+        'y',
+        'u',
+        'i',
+        'o',
+        'p',
+        'A',
+        'S',
+        'D',
+        'F',
+        'G',
+        'H',
+        'J',
+        'K',
+        'L',
+        ':',
+        'Q',
+        'W',
+        'E',
+        'R',
+        'T',
+        'Y',
+        'U',
+        'I',
+        'O',
+        'P',
+      },
+      -- Highlight groups to link to signs and lines; the below configuration refers to demo
+      -- sign_hl typically only defines foreground to possibly be combined with line_hl
+      sign_hl = { 'WarningMsg', 'Title' },
+      -- optional, typically a table of two highlight groups that are alternated between
+      line_hl = { 'CursorLine', 'Normal' },
+      -- options specific to `hop_loop`
+      -- true temporarily disables Telescope selection highlighting
+      clear_selection_hl = false,
+      -- highlight hopped to entry with telescope selection highlight
+      -- note: mutually exclusive with `clear_selection_hl`
+      trace_entry = true,
+      -- jump to entry where hoop loop was started from
+      reset_selection = true,
+    },
     project = {
       base_dir = {
         '~/project',
@@ -51,18 +117,20 @@ telescope.setup({
   },
 })
 
-require('telescope').load_extension('fzf')
-require('telescope').load_extension('session-lens')
-require('telescope').load_extension('projects')
+telescope.load_extension('fzf')
+telescope.load_extension('session-lens')
+telescope.load_extension('hop')
+-- require('telescope').load_extension('projects')
 
 -- mapping
 nnoremap({ '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>" })
 nnoremap({ '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>" })
 nnoremap({ '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>" })
 nnoremap({ '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>" })
+nnoremap({ '<leader>fq', "<cmd>lua require('telescope.builtin').quickfix()<cr>" })
+
+nnoremap({ '<leader>gb', '<cmd>lua require("telescope.builtin").git_bcommits()<cr>' })
+nnoremap({ '<leader>gs', '<cmd>lua require("telescope.builtin").git_status()<cr>' })
+
 nnoremap({ '<leader>sl', "<cmd>:lua require('session-lens').search_session()<cr>" })
-
-nnoremap({ '<leader>h', '<cmd>lua require("telescope.builtin").git_bcommits()<cr>' })
-nnoremap({ '<leader>i', '<cmd>lua require("telescope.builtin").git_status()<cr>' })
-
-nnoremap({ '<leader>fd', '<cmd>:Telescope projects<cr>' })
+-- nnoremap({ '<leader>fd', '<cmd>:Telescope projects<cr>' })
