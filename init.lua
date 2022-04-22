9fba36de49408848e70d4dac0a4d896d66158609
Gilga <pyua87@gmail.com>
2022-04-22 08:17:37 +0300

    Clean

--- init.lua
+++ init.lua
@@ -2,8 +2,3 @@ require('global')
 require('plugins')
 require('keymaps')
 require('settings')
--- require("themes/tokyonight")
--- require('themes/kanagawa')
-
--- dashboard-nvim
---vim.g.dashboard_default_executive = "telescope"
