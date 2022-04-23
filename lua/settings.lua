50ee3ce3204cebc3a606f67f4e061520d4bfbb1f
Gilga <pyua87@gmail.com>
2022-04-23 09:48:19 +0300

    Sorted list

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,89 +1,78 @@
 local g, o, opt = vim.g, vim.o, vim.opt
 local indent = 2
 
--- Better default
--- opt.inccommand = "split"
-opt.splitright = true
-opt.splitbelow = true
-opt.mouse = 'a'
--- opt.showmode = false
-opt.fillchars = { eob = ' ' }
-opt.encoding = 'UTF-8'
-opt.shortmess:append('sI')
-opt.scrolloff = 8
---opt.swapfile = false
---opt.timeout = true
--- Decrease update time
-opt.timeoutlen = 400
-opt.updatetime = 250
+local options = {
+  background = 'dark',
+  backup = false,
+  backupdir = '~/.nvim/backup/',
+  clipboard = 'unnamedplus',
+  cmdheight = 1,
+  completeopt = 'menu,menuone,noselect',
+  cursorline = true,
+  directory = '~/.nvim/tmp/',
+  encoding = 'UTF-8',
+  expandtab = true,
+  fillchars = { eob = ' ' },
+  hidden = true, -- Don't save when switching buffer
+  -- hlsearch = false,
+  ignorecase = true,
+  -- inccommand = "split",
+  lazyredraw = true,
+  mouse = 'a',
+  number = true,
+  numberwidth = 2,
+  -- relativenumber = true,
+  scrolloff = 8,
+  shiftwidth = indent,
+  shortmess = opt.shortmess + 'sI',
+  -- showmode = false,
+  signcolumn = 'yes:1',
+  smartcase = true,
+  smartindent = true,
+  smarttab = true,
+  -- softtabstop = indent,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  tabstop = indent,
+  termguicolors = true,
+  -- timeout = true,
+  timeoutlen = 400,
+  undodir = '/tmp/',
+  undofile = true,
+  updatetime = 250,
+  wrap = false,
+  writebackup = false,
+}
 
--- Better editor UI
-opt.number = true
-opt.numberwidth = 2
---opt.relativenumber = true
-opt.signcolumn = 'yes:1'
-opt.cursorline = true
-
--- Undo and backup options
-opt.backup = false
-opt.writebackup = false
-opt.undofile = true
-opt.swapfile = false
-opt.backupdir = '~/.nvim/backup/'
-opt.directory = '~/.nvim/tmp/'
-opt.undodir = '/tmp/'
-
--- Colors
-opt.termguicolors = true
-opt.background = 'dark'
-
-opt.hidden = true -- Don't save when switching buffer
-opt.smartcase = true
-opt.ignorecase = true
--- o.hlsearch = false
-
--- Tab size options
-opt.expandtab = true
-opt.smarttab = true
-opt.tabstop = indent
-opt.shiftwidth = indent
--- o.softtabstop = indent
-opt.smartindent = true
-opt.wrap = false
-
--- Makes neovim and host OS clipboard play nicely with each other
-opt.clipboard = 'unnamedplus'
-opt.cmdheight = 1
--- When running macros and regexes on a large file, lazy redraw tells neovim not to draw the screen, which greatly speeds it up, upto 6-7x faster
-opt.lazyredraw = true
-
--- Set completeopt to have a better completion experience
-opt.completeopt = 'menu,menuone,noselect'
+for option, value in pairs(options) do
+  opt[option] = value
+end
 
 o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal'
 
--- disable some builtin vim plugins
+-- Disable some builtin vim plugins
 local disabled_built_ins = {
   '2html_plugin',
   'getscript',
   'getscriptPlugin',
   'gzip',
   'logipat',
-  'netrw',
-  'netrwPlugin',
-  'netrwSettings',
-  'netrwFileHandlers',
   'matchit',
   'matchparen',
+  'netrw',
+  'netrwFileHandlers',
+  'netrwPlugin',
+  'netrwSettings',
+  'rrhelper',
+  'spec',
+  'spellfile_plugin',
   'tar',
   'tarPlugin',
-  'rrhelper',
-  'spellfile_plugin',
   'vimball',
   'vimballPlugin',
   'zip',
   'zipPlugin',
-  'spec',
 }
 
 for _, plugin in pairs(disabled_built_ins) do
