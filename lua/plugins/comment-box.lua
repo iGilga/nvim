08adc2737d73aade98ab32fbda87d7ae8d2cd295
Gilga <pyua87@gmail.com>
2022-04-27 15:22:13 +0300

    move config this plugin in lua/plugins

---
+++ lua/plugins/comment-box.lua
@@ -0,0 +1,10 @@
+local keymap = vim.api.nvim_set_keymap
+
+keymap('n', '<Leader>cl', "<Cmd>lua require('comment-box').lbox()<CR>", {})
+keymap('v', '<Leader>cl', "<Cmd>lua require('comment-box').lbox()<CR>", {})
+
+keymap('n', '<Leader>cc', "<Cmd>lua require('comment-box').cbox()<CR>", {})
+keymap('v', '<Leader>cc', "<Cmd>lua require('comment-box').cbox()<CR>", {})
+
+keymap('n', '<Leader>ci', "<Cmd>lua require('comment-box').line()<CR>", {})
+keymap('i', '<-l>', "<Cmd>lua require('comment-box').line()<CR>", {})
