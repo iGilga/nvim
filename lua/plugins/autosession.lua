b1783f4675b57724dc4f487bd62081d158703f94
Gilga <pyua87@gmail.com>
2022-04-27 15:20:42 +0300

    Reset default for autosession

--- lua/plugins/autosession.lua
+++ lua/plugins/autosession.lua
@@ -2,7 +2,8 @@ local setup = {
   auto_save_enabled = false,
   auto_restore_enabled = false,
   auto_session_create_enabled = false,
-  log_level = 'error',
+  log_level = 'info',
+  -- auto_session_suppress_dirs = {'~/', '~/project'}
 }
 
 require('auto-session').setup(setup)
