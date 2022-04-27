40f21a43fddc50c3205839d2a419b6ff5ceb2a1c
Gilga <pyua87@gmail.com>
2022-04-27 15:25:01 +0300

    Commeted path code

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -6,13 +6,17 @@ ls.config.set_config({
   enable_autosnippets = true,
 })
 
-ls.snipets = {
+ls.add_snippets(nil, {
   all = {},
   html = {},
-}
+})
 
-ls.snippets.javascript = ls.snippets.html
-ls.snippets.javascriptreact = ls.snippets.html
-ls.snippets.typescriptreact = ls.snippets.html
+ls.add_snippets('javascript', ls.get_snippets('html'))
+ls.add_snippets('javascriptreact', ls.get_snippets('html'))
+ls.add_snippets('typescriptreact', ls.get_snippets('html'))
 
-require('luasnip/loaders/from_vscode').lazy_load({ include = { 'html' } })
+-- ls.snippets.javascript = ls.snippets.html
+-- ls.snippets.javascriptreact = ls.snippets.html
+-- ls.snippets.typescriptreact = ls.snippets.html
+
+require('luasnip.loaders.from_vscode').lazy_load({ include = { 'html' } })
