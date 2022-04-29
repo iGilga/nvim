d2c734a8ce1dedd73773f13726a8c55ab26b7e47
Gilga <pyua87@gmail.com>
2022-04-29 22:43:03 +0300

    Added logger using notify

---
+++ lua/utils/logger.lua
@@ -0,0 +1,48 @@
+local levels = vim.log.levels
+local config = require('config')
+local u = require('utils')
+
+local ok, load_notify = pcall(require, 'notify')
+
+if ok then
+  load_notify.setup({
+    stages = 'fade',
+    on_open = function(win)
+      vim.api.nvim_win_set_config(win, { border = config.border })
+    end,
+    timeout = 2000,
+  })
+  vim.notify = load_notify
+end
+
+local M = {}
+
+M.init = function() end
+
+M.info = function(text, opt)
+  vim.notify(text, levels.INFO, opt)
+end
+
+M.warn = function(text, opt)
+  vim.notify(text, levels.WARN, opt)
+end
+
+M.error = function(text, opt)
+  vim.notify(text, levels.WARN, opt)
+end
+
+local renderOption = { render = 'minimal' }
+
+M.minfo = function(text)
+  vim.notify(text, 'info', renderOption)
+end
+
+M.mwarn = function(text)
+  vim.notify(text, levels.WARN, renderOption)
+end
+
+M.merror = function(text)
+  vim.notify(text, levels.WARN, renderOption)
+end
+
+return M
