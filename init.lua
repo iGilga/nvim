91c41a9c2ddd3a47b1d4c04dbd82a11106867a30
Gilga <pyua87@gmail.com>
2022-04-29 22:40:33 +0300

    enable impatient for fitst init, config filetype.nvim

--- init.lua
+++ init.lua
@@ -1,4 +1,24 @@
+pcall(require, 'impatient')
+-- pcall(function()
+--   require('impatient').enable_profile()
+-- end)
+
+local o, c, g = vim.opt, vim.cmd, vim.g
+
+local ok_filetype, filetype = pcall(require, 'filetype')
+
+if ok_filetype then
+  filetype.setup({})
+  -- deactivate vim based filetype detection
+  g.did_load_filetype = 1
+end
+
+require('settings')
+
+c('filetype plugin indent on')
+
 require('global')
 require('plugins')
-require('keymaps')
-require('settings')
+require('mapping')
+require('autocmds')
+require('highlight')
