f3514c2a0e32091b1fd01900c11d8be1ae9f4950
Gilga <pyua87@gmail.com>
2022-05-05 00:12:48 +0300

    Added lowercase

--- lua/plugins/todocomments.lua
+++ lua/plugins/todocomments.lua
@@ -1,7 +1,28 @@
 local colors = require('theme.color.kanagawa')
 local nnoremap = require('utils.keymap').nnoremap
 
-local setup = {}
+local setup = {
+  keywords = {
+    TODO = {
+      alt = { 'TODO', 'todo' },
+    },
+    NOTE = {
+      alt = { 'NOTE', 'note' },
+    },
+    FIX = {
+      alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE', 'fix', 'fixme', 'fixit' },
+    },
+    WARN = {
+      alt = { 'WARN', 'XXX', 'warn', 'xxx' },
+    },
+    PERF = {
+      alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE', 'optim', 'performance', 'optimize' },
+    },
+    HACK = {
+      alt = { 'HACK', 'hack' },
+    },
+  },
+}
 
 nnoremap({ '<leader>ft', ':TodoTelescope<cr>' })
 
