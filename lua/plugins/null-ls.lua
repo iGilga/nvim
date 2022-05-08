2a9803d2dbcfdd009e329661833b9ea1f6c59cfe
Gilga <pyua87@gmail.com>
2022-05-09 01:27:21 +0300

    Refactoring and add autocommand :Format

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,5 +1,7 @@
 local nullls = require('null-ls')
--- local defaultAttach = require('lsp.servers.default').on_attach
+local code_actions = nullls.builtins.code_actions
+local diagnostics = nullls.builtins.diagnostics
+local formatting = nullls.builtins.formatting
 
 local has_eslint_config = function(utils)
   local files = {
@@ -9,44 +11,78 @@ local has_eslint_config = function(utils)
     '.eslintrc.cjs',
     '.eslintrc.yaml',
     '.eslintrc.yml',
-    -- 'package.json',
   }
   return utils.has_file(files) or utils.root_has_file(files)
 end
 
+local has_prettier_config = function(utils)
+  local files = {
+    '.prettierrc',
+    '.prettierrc.json',
+    '.prettierrc.yml',
+    '.prettierrc.yaml',
+    '.prettierrc.json',
+    '.prettierrc.js',
+    '.prettierrc.cjs',
+    '.prettier.config.js',
+    '.prettier.config.cjs',
+    '.prettierrc.toml',
+  }
+
+  return utils.has_file(files) or utils.root_has_file(files)
+end
+
 local sources = {
-  nullls.builtins.code_actions.eslint_d.with({
+  code_actions.eslint_d.with({
     condition = has_eslint_config,
     prefer_local = 'node_modules/.bin',
   }),
-  nullls.builtins.diagnostics.eslint_d.with({
+  code_actions.gitsigns,
+  diagnostics.eslint_d.with({
     condition = has_eslint_config,
     prefer_local = 'node_modules/.bin',
   }),
-  nullls.builtins.formatting.eslint_d.with({
+  diagnostics.yamllint,
+  formatting.eslint_d.with({
     condition = has_eslint_config,
     prefer_local = 'node_modules/.bin',
   }),
-  nullls.builtins.code_actions.gitsigns,
-  nullls.builtins.formatting.prettierd,
-  nullls.builtins.formatting.stylua.with({
-    extra_args = { '--config-path', vim.fn.expand('~/.config/stylua/stylua.toml') },
+  formatting.prettierd.with({
+    -- condition = function()
+    --   return not has_prettier_config
+    -- end,
+    env = {
+      PRETTIERD_DEFAULT_CONFIG = vim.fn.expand('~/.config/nvim/lintercfg/prettierrc'),
+    },
+    disabled_filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue' },
+  }),
+  formatting.shfmt.with({
+    extra_args = { "-sr" }
+  }),
+  formatting.stylua.with({
+    extra_args = { '--config-path', vim.fn.expand('~/.config/nvim/lintercfg/stylua.toml') },
   }),
 }
 
--- local function on_attachCustom(client)
---   if client.resolved_capabilities.document_formatting then
---     vim.cmd([[
---             augroup LspFormatting
---                 autocmd! * <buffer>
---                 autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
---             augroup END
---             ]])
---   end
--- end
+-- local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
+local function onAttachCustom(client, bufnr)
+  vim.api.nvim_create_user_command('Format', vim.lsp.buf.formatting, {})
+  -- if client.supports_method('textDocument/formatting') then
+  -- if client.resolved_capabilities.document_formatting then
+  --   vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  --   vim.api.nvim_create_autocmd('BufWritePre', {
+  --     group = augroup,
+  --     buffer = bufnr,
+  --     -- on 0.8, you should use vim.lsp.buf.format instead
+  --     callback = vim.lsp.buf.formatting,
+  --   })
+  -- end
+end
 
 local setup = {
-  -- on_attach = defaultAttach,
+  debug = false,
+  on_attach = onAttachCustom,
   sources = sources,
 }
 
