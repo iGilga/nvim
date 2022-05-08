125c335d9e1ad2ce2ec329bc6c33646d50cc5368
Gilga <pyua87@gmail.com>
2022-05-09 01:26:31 +0300

    Added check empty result.

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -1,6 +1,6 @@
 local lsp = vim.lsp
 local border = require('config').border
-local get_relative_path = require('utils').get_relative_path
+-- local get_relative_path = require('utils').get_relative_path
 local logger = require('utils.logger')
 
 lsp.handlers['textDocument/hover'] = lsp.with(lsp.handlers.hover, {
@@ -12,11 +12,11 @@ lsp.handlers['textDocument/signatureHelp'] = lsp.with(lsp.handlers.signature_hel
 })
 
 lsp.handlers['textDocument/formatting'] = function(_, result, ctx)
-  if result then
+  if result and not vim.tbl_isempty(result) then
     local client = lsp.get_client_by_id(ctx.client_id)
     lsp.util.apply_text_edits(result, ctx.bufnr, client.offset_encoding)
-    -- local msg = ('[LSP]Formatting done. [%s]'):format(get_relative_path(ctx.params.textDocument.uri))
-    local msg = '[LSP]Formatting done.'
+    local msg = ('[LSP][%s]Formatting done'):format(client.name)
+    -- local msg = '[LSP]Formatting done.'
     logger.minfo(msg)
   end
 end
