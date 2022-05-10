ce9c3907ec7e01c1008733afa71f84ce51090b96
Gilga <pyua87@gmail.com>
2022-05-10 06:03:28 +0300

    Config highlight for nvim_set_hl

--- lua/theme/color/kanagawa.lua
+++ lua/theme/color/kanagawa.lua
@@ -18,4 +18,27 @@ M.colors = {
   gray = theme.sumiInk4,
 }
 
+M.groups = {
+  NotifyINFOBorder = { fg = M.colors.green },
+  NotifyINFOIcon = { fg = M.colors.green },
+  NotifyINFOTitle = { fg = M.colors.green },
+  NotifyINFOBody = { fg = M.colors.white },
+  NotifyWARNBorder = { fg = M.colors.yellow },
+  NotifyWARNIcon = { fg = M.colors.yellow },
+  NotifyWARNTitle = { fg = M.colors.yellow },
+  NotifyWARNBody = { fg = M.colors.white },
+  NotifyERRORBorder = { fg = M.colors.red },
+  NotifyERRORIcon = { fg = M.colors.red },
+  NotifyERRORTitle = { fg = M.colors.red },
+  NotifyERRORBody = { fg = M.colors.white },
+  NotifyTRACEBorder = { fg = M.colors.purple },
+  NotifyTRACEIcon = { fg = M.colors.purple },
+  NotifyTRACETitle = { fg = M.colors.purple },
+  NotifyTRACEBody = { fg = M.colors.white },
+  AlphaHeader = { fg = M.colors.green },
+  AlphaShortcuts = { fg = M.colors.bg, bg = M.colors.green },
+  AlphaButton = { fg = M.colors.white },
+  AlphaIcon = { fg = M.colors.green },
+}
+
 return M
