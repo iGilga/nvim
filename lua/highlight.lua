aa6447d1dd27b38d9568468fecc9683e7c8a6e72
Gilga <pyua87@gmail.com>
2022-05-10 06:03:53 +0300

    Change from vim to nvim_set_hl

--- lua/highlight.lua
+++ lua/highlight.lua
@@ -5,36 +5,10 @@ if not ok then
   return false
 end
 
-local colors = kanagawa.colors
-
-local bg = function(group, color)
-  c('highlight  ' .. group .. ' guibg=' .. color)
+local load = function(highlights)
+  for group, hl in pairs(highlights) do
+    vim.api.nvim_set_hl(0, group, hl)
+  end
 end
 
-local fg = function(group, color)
-  c('hi ' .. group .. ' guifg=' .. color)
-end
-
-local bgfg = function(group, bgColor, fgColor)
-  c('highlight ' .. group .. ' guibg=' .. bgColor .. ' guifg=' .. fgColor)
-end
-
-fg('NotifyINFOBorder', colors.green)
-fg('NotifyINFOIcon', colors.green)
-fg('NotifyINFOTitle', colors.green)
-fg('NotifyINFOBody', colors.white)
-
-fg('NotifyWARNBorder', colors.yellow)
-fg('NotifyWARNIcon', colors.yellow)
-fg('NotifyWARNTitle', colors.yellow)
-fg('NotifyWARNBody', colors.white)
-
-fg('NotifyERRORBorder', colors.red)
-fg('NotifyERRORIcon', colors.red)
-fg('NotifyERRORTitle', colors.red)
-fg('NotifyERRORTBody', colors.white)
-
-fg('NotifyTRACEBorder', colors.purple)
-fg('NotifyTRACEIcon', colors.purple)
-fg('NotifyTRACETitle', colors.purple)
-fg('NotifyTRACETBody', colors.white)
+load(kanagawa.groups)
