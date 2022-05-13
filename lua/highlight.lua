5d552e042ebce14f8e8caf77736df7072de61023
Gilga <pyua87@gmail.com>
2022-05-13 07:10:07 +0300

    Deactivate custom highlight.(use config for kanagawa theme)

--- lua/highlight.lua
+++ lua/highlight.lua
@@ -1,13 +1,13 @@
-local ok, kanagawa = pcall(require, 'theme.color.kanagawa')
+-- local ok, kanagawa = pcall(require, 'theme.color.kanagawa')
+--
+-- if not ok then
+--   return false
+-- end
+--
+-- local load = function(highlights)
+--   for group, hl in pairs(highlights) do
+--     vim.api.nvim_set_hl(0, group, hl)
+--   end
+-- end
 
-if not ok then
-  return false
-end
-
-local load = function(highlights)
-  for group, hl in pairs(highlights) do
-    vim.api.nvim_set_hl(0, group, hl)
-  end
-end
-
-load(kanagawa.groups)
+-- load(kanagawa.groups)
