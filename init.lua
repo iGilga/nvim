a3304b3d77bea67f1607f8897454cb7b648e6be7
Gilga <pyua87@gmail.com>
2022-05-13 22:27:02 +0300

    Check version neovim. Replace vim.cmd to vim.api.nvim_command.

--- init.lua
+++ init.lua
@@ -1,21 +1,31 @@
-pcall(require, 'impatient')
+if vim.fn.has('nvim-0.7') == 0 then
+  error('Need Neovim v0.7+')
+end
+
+do
+  local ok, _ = pcall(require, 'impatient')
+
+  if not ok then
+    vim.notify('impatient.nvim not installed', vim.log.levels.WARN)
+  end
+end
+
 -- pcall(function()
 --   require('impatient').enable_profile()
 -- end)
-
-local o, c, g = vim.opt, vim.cmd, vim.g
 
 local ok_filetype, filetype = pcall(require, 'filetype')
 
 if ok_filetype then
   filetype.setup({})
   -- deactivate vim based filetype detection
-  g.did_load_filetype = 1
+  vim.g.did_load_filetype = 1
 end
 
 require('settings')
 
-c('filetype plugin indent on')
+vim.api.nvim_command('filetype plugin indent on')
+-- vim.cmd('filetype plugin indent on')
 
 require('global')
 require('plugins')
