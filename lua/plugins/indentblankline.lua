cfc2ca8bcf03a2c3a4bf9ab07d29aaf15363463f
Gilga <pyua87@gmail.com>
2022-05-19 03:19:30 +0300

    create module for indentblankline

---
+++ lua/plugins/indentblankline.lua
@@ -0,0 +1,22 @@
+local setup = {
+  char_list = { '|', '¦', '┆', '┊' },
+  show_current_context = true,
+  -- show_current_context_start = true,
+  -- space_char_blankline = ' ',
+  buftype_exclude = {
+    'terminal',
+    'prompt',
+    'nofile',
+  },
+  filetype_exclude = {
+    'help',
+    'packer',
+    'lspinfo',
+    'alpha',
+    'NnnExplorer',
+    'NnnPicker',
+    'markdown',
+  },
+}
+
+require('indent_blankline').setup(setup)
