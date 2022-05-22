81a2ff3e93a921e99f9f425f672db296616784eb
Gilga <pyua87@gmail.com>
2022-05-22 08:44:02 +0300

    Custom render for notify.

--- lua/utils/logger.lua
+++ lua/utils/logger.lua
@@ -13,6 +13,24 @@ if ok then
   vim.notify = load_notify
 end
 
+local namespace = vim.api.nvim_create_namespace('nvim-notify')
+
+local customRender = function(bufnr, notif, highlights)
+  local left_icon = notif.icon .. ' '
+  local message = { string.rep(' ', 3) .. notif.message[1] }
+  vim.api.nvim_buf_set_lines(bufnr, 1, -1, false, message)
+  vim.api.nvim_buf_set_extmark(bufnr, namespace, 0, 0, {
+    virt_text = {
+      { ' ' },
+      { left_icon, highlights.icon },
+      { notif.message[2], highlights.icon },
+    },
+    priority = 50,
+    virt_text_win_col = 0,
+    hl_group = highlights.body,
+  })
+end
+
 local M = {}
 
 M.init = function() end
@@ -29,18 +47,18 @@ M.error = function(text, opt)
   vim.notify(text, levels.WARN, opt)
 end
 
-local renderOption = { render = 'minimal' }
+local renderOption = { render = customRender }
 
-M.minfo = function(text)
-  vim.notify(text, 'info', renderOption)
+M.minfo = function(title, message)
+  vim.notify({ message, title }, 'info', renderOption)
 end
 
-M.mwarn = function(text)
-  vim.notify(text, levels.WARN, renderOption)
+M.mwarn = function(title, message)
+  vim.notify({ message, title }, 'warn', renderOption)
 end
 
-M.merror = function(text)
-  vim.notify(text, levels.WARN, renderOption)
+M.merror = function(title, message)
+  vim.notify({ message, title }, 'error', renderOption)
 end
 
 return M
