4429b1d82b526a7718467c7dfeacaee00e95e763
Gilga <pyua87@gmail.com>
2022-05-23 03:00:57 +0300

    Rewrite custom render for nvim-notify. Custom render now default.

--- lua/utils/logger.lua
+++ lua/utils/logger.lua
@@ -1,75 +1,79 @@
 local levels = vim.log.levels
 
-local ok, load_notify = pcall(require, 'notify')
+local ok, moduleNotify = pcall(require, 'notify')
 
-if ok then
-  load_notify.setup({
-    stages = 'fade',
-    on_open = function(win)
-      P(win)
-      -- [ "╔", "═" ,"╗", "║", "╝", "═", "╚", "║" ]
-      vim.api.nvim_win_set_config(win, { border = { '', ' ', '', '', '', ' ', '', '' } })
-    end,
-    timeout = 2000,
-  })
-  vim.notify = load_notify
+if not ok then
+  return false
 end
 
 local namespace = vim.api.nvim_create_namespace('nvim-notify')
 
 local customRender = function(bufnr, notif, highlights)
   local hlSign = 'Notify' .. notif.level .. 'Sign'
-  local title = notif.message[2]
-  local message = notif.message[1]
-  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { '', '' })
-  vim.api.nvim_buf_set_extmark(bufnr, namespace, 0, 0, {
-    virt_text = {
-      { ' ', hlSign },
-      { ' ' },
-      { title, highlights.icon },
-    },
-    priority = 10,
-    virt_text_win_col = 0,
-  })
-  vim.api.nvim_buf_set_extmark(bufnr, namespace, 1, 0, {
-    virt_text = {
-      { ' ', hlSign },
-      { ' ' },
-      { message, highlights.body },
-    },
-    priority = 50,
-    virt_text_win_col = 0,
-  })
+  local title = notif.title[1]
+  local message = notif.message
+
+  local set_config = function(text, hl, priority)
+    return {
+      virt_text = {
+        { ' ', hlSign },
+        { ' ' },
+        { text, hl },
+      },
+      priority = priority,
+      virt_text_win_col = 0,
+    }
+  end
+  local lines = {}
+
+  if not (title == nil or title == '') then
+    lines[1] = set_config(title, highlights.icon, 10)
+  end
+
+  local count = #lines
+  local spaces = {}
+
+  for i = 1, #message do
+    lines[count + i] = set_config(message[i], highlights.body, 50)
+  end
+  for i = 1, #lines do
+    spaces[i] = ''
+  end
+
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, spaces)
+
+  for i = 1, #lines do
+    vim.api.nvim_buf_set_extmark(bufnr, namespace, i - 1, 0, lines[i])
+  end
 end
+
+moduleNotify.setup({
+  stages = 'fade',
+  render = customRender,
+  on_open = function(win)
+    -- [ "╔", "═" ,"╗", "║", "╝", "═", "╚", "║" ]
+    vim.api.nvim_win_set_config(win, { border = { '', ' ', '', '', '', ' ', '', '' } })
+  end,
+  timeout = 12000,
+})
+vim.notify = moduleNotify
 
 local M = {}
 
-M.init = function() end
-
-M.info = function(text, opt)
-  vim.notify(text, levels.INFO, opt)
+M.info = function(text, title)
+  vim.notify(text, levels.INFO, { title = title })
 end
 
-M.warn = function(text, opt)
-  vim.notify(text, levels.WARN, opt)
+M.warn = function(text, title)
+  vim.notify(text, levels.WARN, { title = title })
 end
 
-M.error = function(text, opt)
-  vim.notify(text, levels.WARN, opt)
+M.error = function(text, title)
+  vim.notify(text, levels.ERROR, { title = title })
 end
 
-local customOption = { render = customRender }
-
-M.minfo = function(title, message)
-  vim.notify({ message, title }, 'info', customOption)
-end
-
-M.mwarn = function(title, message)
-  vim.notify({ message, title }, 'warn', customOption)
-end
-
-M.merror = function(title, message)
-  vim.notify({ message, title }, 'error', customOption)
+M.hint = function(text, title)
+  vim.notify(text, levels.TRACE, { title = title })
 end
 
 return M
