03274ca8db2f736af2918f6c4d480244e5951776
Gilga <pyua87@gmail.com>
2022-06-24 04:47:11 +0300

    deactive vim base filetype detection

--- init.lua
+++ init.lua
@@ -19,7 +19,8 @@ local ok_filetype, filetype = pcall(require, 'filetype')
 if ok_filetype then
   filetype.setup({})
   -- deactivate vim based filetype detection
-  vim.g.did_load_filetype = 1
+  vim.g.do_filetype_lua = 1
+  vim.g.did_load_filetype = 0
 end
 
 require('settings')
@@ -31,4 +32,3 @@ require('global')
 require('plugins')
 require('mapping')
 require('autocmds')
--- require('highlight')
