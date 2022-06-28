7cb56b446af8d1d6ccf5fefb6c17ba91b59f52dd
Gilga <pyua87@gmail.com>
2022-06-28 03:09:56 +0300

    Reorganized

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -16,30 +16,47 @@ map('n', '<leader>s', ':w<cr>', { desc = 'save' })
 -- r highlight
 map('n', '<F5>', ':nohl<cr>', { desc = 'no highlights' })
 
--- barbar.nvim
+-- Packer
+map('n', '<leader>pc', '<cmd>PackerCompile<cr>', { desc = 'Packer Compile' })
+map('n', '<leader>pi', '<cmd>PackerInstall<cr>', { desc = 'Packer Install' })
+map('n', '<leader>ps', '<cmd>PackerSync<cr>', { desc = 'Packer Sync' })
+map('n', '<leader>pS', '<cmd>PackerStatus<cr>', { desc = 'Packer Status' })
+map('n', '<leader>pu', '<cmd>PackerUpdate<cr>', { desc = 'Packer Update' })
+
 local opts = function(desc)
   return { noremap = true, silent = true, desc = desc }
 end
 
-map('n', '<a-k>', ':BufferNext<cr>', opts('next buffer'))
-map('n', '<a-j>', ':BufferPrevious<cr>', opts('prev buffer'))
+map('n', '<a-k>', ':BufferLineCycleNext<cr>', opts('next buffer'))
+map('n', '<a-j>', ':BufferLineCyclePrev<cr>', opts('prev buffer'))
 -- nnoremap({ '<A-0>', ':BufferGotoLast<cr>', opts })
-map('n', 'ts', ':BufferClose<cr>', opts('close buffer'))
-map('n', 'tt', ':BufferPick<cr>', opts('pick buffer'))
-map('n', 'tn', ':BufferOrderByBufferNumber<cr>', opts('sort buffer number'))
-map('n', 'td', ':BufferOrderByDirectory<cr>', opts('sort buffer directory'))
-map('n', 'tl', ':BufferOrderByLanguage<cr>', opts('sort buffer language'))
+map('n', 'ts', ':BufferLinePickClose<cr>', opts('close buffer'))
+map('n', 'tss', ':bdelete<cr>', opts('close buffer'))
+map('n', 'tt', ':BufferLinePick<cr>', opts('pick buffer'))
+map('n', 'tn', function()
+  require('bufferline').sort_buffers_by(function(buf_a, buf_b)
+    return buf_a.id < buf_b.id
+  end)
+end, opts('sort buffer number'))
+map('n', 'td', function()
+  require('bufferline').sort_buffers_by('directory')
+end, opts('sort buffer directory'))
+map('n', 'te', function()
+  require('bufferline').sort_buffers_by('extension')
+end, opts('sort buffer language'))
 
 -- hop mapping
 map('n', '<leader>w', "<cmd>lua require'hop'.hint_words()<cr>", { desc = 'motion word' })
 map('n', '<leader>l', "<cmd>lua require'hop'.hint_lines()<cr>", { desc = 'motion line' })
 
+-- nnn.nvim
 map('t', '<c-t>', '<cmd>:NnnPicker<cr>', { desc = 'open nnn' })
 map('n', '<c-t>', '<cmd>:NnnPicker<cr>', { desc = 'open nnn' })
 
 -- LazyGit
 map('n', '<leader>ll', ':LazyGit<cr>', { desc = 'open lazygit' })
 
+-- SessionManager
 map('n', '<leader>sl', '<cmd>SessionManager! load_last_session<cr>', { desc = 'Load last session' })
 map('n', '<leader>oo', '<cmd>SessionManager! save_current_session<cr>', { desc = 'Save this session' })
 map('n', '<leader>od', '<cmd>SessionManager! delete_session<cr>', { desc = 'Delete session' })
@@ -47,3 +64,17 @@ map('n', '<leader>sf', '<cmd>SessionManager! load_session<cr>', { desc = 'Search
 map('n', '<leader>o.', '<cmd>SessionManager! load_current_dir_session<cr>', {
   desc = 'Load current directory session',
 })
+
+map('n', '<leader>zf', ':lua require("telescope").extensions.zettel.find_zettel()<CR>')
+map('n', '<leader>zg', ':lua require("telescope").extensions.zettel.grep_zettels()<CR>')
+
+map('n', '<Leader>bb', "<Cmd>lua require('comment-box').lbox(2)<CR>", { desc = 'box' })
+map('v', '<Leader>bb', "<Cmd>lua require('comment-box').lbox(2)<CR>", { desc = 'box' })
+
+-- centered adapted box with centered text
+map('n', '<Leader>bc', "<Cmd>lua require('comment-box').accbox(2)<CR>", { desc = 'centred box' })
+map('v', '<Leader>bc', "<Cmd>lua require('comment-box').accbox(2)<CR>", { desc = 'centred box' })
+
+-- centered line
+map('n', '<Leader>bl', "<Cmd>lua require('comment-box').cline(1)<CR>", { desc = 'line' })
+map('i', '<M-l>', "<Cmd>lua require('comment-box').cline(1)<CR>", { desc = 'line' })
