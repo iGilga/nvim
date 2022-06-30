6535863cec706da70d5fea11a5bd169fa172e93a
Gilga <pyua87@gmail.com>
2022-06-30 03:11:47 +0300

    Added autocmd for openra.

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,3 +1,4 @@
+-- local logger = require('utils.logger')
 -- Auto sync plugins on save of plugins.lua
 -- vim.api.nvim_create_autocmd("BufWritePost", { pattern = "plugins.lua", command = "source <afile> | PackerSync" })
 --
@@ -20,9 +21,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 vim.api.nvim_create_autocmd({ 'BufEnter' }, { command = ':lua require("lazygit.utils").project_root_dir()' })
 
-vim.api.nvim_create_autocmd( 'BufRead' , { pattern = '*/openra/*', command = 'setlocal noexpandtab tabstop=4 shiftwidth=4' })
+vim.api.nvim_create_autocmd(
+  'BufRead',
+  { pattern = '*/openra/*', command = 'setlocal noexpandtab tabstop=4 shiftwidth=4' }
+)
 
-local wrap_md_group = vim.api.nvim_create_augroup("Markdown Wrap Settings", { clear = true })
+local wrap_md_group = vim.api.nvim_create_augroup('Markdown Wrap Settings', { clear = true })
 
 vim.api.nvim_create_autocmd('BufEnter', {
   pattern = { '*.md' },
