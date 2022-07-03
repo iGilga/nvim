05de3c2e757194e40013cbdafeb1d0e58fb99ed7
Gilga <pyua87@gmail.com>
2022-07-03 03:32:34 +0300

    Empty separators and shoter widht tab.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,13 +1,17 @@
 local setup = {
   options = {
-    offsets = {
-      { filetype = 'NvimTree', text = '', padding = 1 },
-      { filetype = 'neo-tree', text = '', padding = 1 },
-      { filetype = 'Outline', text = '', padding = 1 },
-    },
-    separator_style = 'thin',
+    -- offsets = {
+    --   { filetype = 'NvimTree', text = '', padding = 1 },
+    --   { filetype = 'neo-tree', text = '', padding = 1 },
+    --   { filetype = 'Outline', text = '', padding = 1 },
+    -- },
+    separator_style = { '', '' },
     show_buffer_close_icons = false,
+    show_tab_indicators = false,
     show_close_icon = false,
+    max_name_length = 14,
+    max_prefix_length = 8,
+    tab_size = 16,
   },
 }
 
