f3207a43ecaaaa7eadd95d0d6d82f0778c76d5e5
Gilga <pyua87@gmail.com>
2022-07-06 07:44:01 +0300

    color for hop

---
+++ lua/theme/plugins/hop.lua
@@ -0,0 +1,8 @@
+return {
+  HopNextKey = { fg = C.peachRed, bold = true },
+  HopNextKey1 = { fg = C.boatYellow2, bold = true },
+  HopNextKey2 = { fg = C.boatYellow1 },
+  HopUnmatched = { fg = C.fujiGray },
+  HopCursor = { link = 'Cursor' },
+  HopPreview = { fg = C.surimiOrange, bold = true },
+}
