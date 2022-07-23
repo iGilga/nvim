d8f11d80c40a2f94b02867cc62e7a123f6cc4f48
Gilga <pyua87@gmail.com>
2022-07-23 13:18:23 +0300

    Replace desc {} to opts()

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -46,56 +46,66 @@ map('n', 'te', function()
 end, opts('sort buffer language'))
 
 -- hop mapping
+map('n', '<leader>w', "<cmd>lua require'hop'.hint_words()<cr>", opts('Hop word'))
+map('n', '<leader>l', "<cmd>lua require'hop'.hint_lines_skip_whitespace()<cr>", opts('Hop line skip whitespace'))
+
 map('n', 's', function()
-  return require('hop').hint_char1({ direction = require('hop.hint').HintDirection.AFTER_CURSOR })
-end, { desc = 'Hop char2 after curso' })
+  return require('hop').hint_char1({
+    direction = require('hop.hint').HintDirection.AFTER_CURSOR,
+  })
+end, opts('Hop char2 after cursor'))
+
 map('n', 'S', function()
-  return require('hop').hint_char1({ direction = require('hop.hint').HintDirection.BEFORE_CURSOR })
-end, { desc = 'Hop char2 before cursor' })
-map('n', '<leader>l', "<cmd>lua require'hop'.hint_lines_skip_whitespace()<cr>", { desc = 'Hop line skip whitespace' })
+  return require('hop').hint_char1({
+    direction = require('hop.hint').HintDirection.BEFORE_CURSOR,
+  })
+end, opts('Hop char2 before cursor'))
 
 map('n', 'f', function()
   return require('hop').hint_char1({
     direction = require('hop.hint').HintDirection.AFTER_CURSOR,
     current_line_only = true,
   })
-end, { desc = 'hop hint curline' })
+end, opts('hop hint curline after'))
+
 map('n', 'F', function()
   return require('hop').hint_char1({
     direction = require('hop.hint').HintDirection.BEFORE_CURSOR,
     current_line_only = true,
   })
-end, { desc = 'hop hint curline' })
+end, opts('hop hint curline before'))
+
 map('n', 't', function()
   return require('hop').hint_char1({
     direction = require('hop.hint').HintDirection.AFTER_CURSOR,
     current_line_only = true,
     hint_offset = -1,
   })
-end, { desc = 'hop hint curline before target' })
+end, opts('hop hint curline before target'))
+
 map('n', 'T', function()
   return require('hop').hint_char1({
     direction = require('hop.hint').HintDirection.BEFORE_CURSOR,
     current_line_only = true,
     hint_offset = -1,
   })
-end, { desc = 'hop hint curline before target' })
+end, opts('hop hint curline before target'))
 map('n', '<leader>h', function()
   return require('hop').hint_patterns()
-end, { desc = 'hop hint with pattern' })
+end, opts('hop hint with pattern'))
 
 -- nnn.nvim
-map('t', '<c-t>', '<cmd>:NnnPicker<cr>', { desc = 'open nnn' })
-map('n', '<c-t>', '<cmd>:NnnPicker<cr>', { desc = 'open nnn' })
+map('t', '<c-t>', '<cmd>:NnnPicker<cr>', opts('open nnn'))
+map('n', '<c-t>', '<cmd>:NnnPicker<cr>', opts('open nnn'))
 
 -- LazyGit
-map('n', '<leader>ll', ':LazyGit<cr>', { desc = 'open lazygit' })
+map('n', '<leader>ll', ':LazyGit<cr>', opts('open lazygit'))
 
 -- SessionManager
-map('n', '<leader>sl', '<cmd>SessionManager! load_last_session<cr>', { desc = 'Load last session' })
-map('n', '<leader>oo', '<cmd>SessionManager! save_current_session<cr>', { desc = 'Save this session' })
-map('n', '<leader>od', '<cmd>SessionManager! delete_session<cr>', { desc = 'Delete session' })
-map('n', '<leader>sf', '<cmd>SessionManager! load_session<cr>', { desc = 'Search sessions' })
+map('n', '<leader>sl', '<cmd>SessionManager! load_last_session<cr>', opts('Load last session'))
+map('n', '<leader>oo', '<cmd>SessionManager! save_current_session<cr>', opts('Save this session'))
+map('n', '<leader>od', '<cmd>SessionManager! delete_session<cr>', opts('Delete session'))
+map('n', '<leader>sf', '<cmd>SessionManager! load_session<cr>', opts('Search sessions'))
 map('n', '<leader>o.', '<cmd>SessionManager! load_current_dir_session<cr>', {
   desc = 'Load current directory session',
 })
@@ -103,13 +113,15 @@ map('n', '<leader>o.', '<cmd>SessionManager! load_current_dir_session<cr>', {
 map('n', '<leader>zf', ':lua require("telescope").extensions.zettel.find_zettel()<CR>')
 map('n', '<leader>zg', ':lua require("telescope").extensions.zettel.grep_zettels()<CR>')
 
-map('n', '<Leader>bb', "<Cmd>lua require('comment-box').lbox(2)<CR>", { desc = 'box' })
-map('v', '<Leader>bb', "<Cmd>lua require('comment-box').lbox(2)<CR>", { desc = 'box' })
+map('n', '<Leader>bb', "<Cmd>lua require('comment-box').lbox(2)<CR>", opts('box'))
+map('v', '<Leader>bb', "<Cmd>lua require('comment-box').lbox(2)<CR>", opts('box'))
 
 -- centered adapted box with centered text
-map('n', '<Leader>bc', "<Cmd>lua require('comment-box').accbox(2)<CR>", { desc = 'centred box' })
-map('v', '<Leader>bc', "<Cmd>lua require('comment-box').accbox(2)<CR>", { desc = 'centred box' })
+map('n', '<Leader>bc', "<Cmd>lua require('comment-box').accbox(2)<CR>", opts('centred box'))
+map('v', '<Leader>bc', "<Cmd>lua require('comment-box').accbox(2)<CR>", opts('centred box'))
 
 -- centered line
-map('n', '<Leader>bl', "<Cmd>lua require('comment-box').cline(1)<CR>", { desc = 'line' })
-map('i', '<M-l>', "<Cmd>lua require('comment-box').cline(1)<CR>", { desc = 'line' })
+map('n', '<Leader>bl', "<Cmd>lua require('comment-box').cline(1)<CR>", opts('line'))
+map('i', '<M-l>', "<Cmd>lua require('comment-box').cline(1)<CR>", opts('line'))
+
+map('n', '<C-x>', ':ColorizerToggle<cr>', opts('Toogle colorizer'))
