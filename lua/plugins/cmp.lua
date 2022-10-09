7f063dd95453182ec9bc270b3b3ac12950e0b783
Gilga <pyua87@gmail.com>
2022-10-09 03:58:30 +0300

    Enable mapping for cmp-cmdline and limited count item on list.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -93,13 +93,15 @@ vim.cmd([[
 cmp.setup(setup)
 
 cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = 'path' },
-    { name = 'cmdline' },
+    { name = 'cmdline', max_item_count = 15 },
   }),
 })
 
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = 'buffer' },
   },
