87e4aaf65451f464c831ecc9dabbe80ae9b0f481
Gilga <pyua87@gmail.com>
2022-10-19 05:58:41 +0300

    Change colors for TSDefinition.

--- lua/theme/base.lua
+++ lua/theme/base.lua
@@ -2,4 +2,6 @@ return {
   NormalFloat = { fg = C.white, bg = C.dark3 },
   FloatBorder = { fg = C.gray, bg = C.dark3 },
   CBorder = { fg = C.green, bg = C.green },
+  TSDefinitionUsage = { fg = C.bg, bg = C.green },
+  TSDefinition = { fg = C.bg, bg = C.yellow },
 }
