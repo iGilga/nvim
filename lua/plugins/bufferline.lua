9e7d27e22ecc750778d57fb9dbd6dfe24a354aa7
Gilga <pyua87@gmail.com>
2022-10-29 16:05:56 +0300

    Added offsets for nvim-tree.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,10 +1,8 @@
 local setup = {
   options = {
-    -- offsets = {
-    --   { filetype = 'NvimTree', text = '', padding = 1 },
-    --   { filetype = 'neo-tree', text = '', padding = 1 },
-    --   { filetype = 'Outline', text = '', padding = 1 },
-    -- },
+    offsets = {
+      { filetype = 'NvimTree', text = '', padding = 1 },
+    },
     separator_style = { '', '' },
     show_buffer_close_icons = false,
     show_tab_indicators = false,
