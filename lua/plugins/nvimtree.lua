695895c107323635c47d38311028ca459a674a2b
Gilga <pyua87@gmail.com>
2022-10-31 19:08:59 +0300

    Config for nvim-tree.

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,16 +1,19 @@
-local nnoremap = require("utils.keymap").nnoremap
-
-vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
-vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_respect_buf_cwd = 1
-
-require("nvim-tree").setup {
-  auto_close = true,
-  update_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_cwd = true
-  }
+local setup = {
+  view = {
+    hide_root_folder = true,
+    signcolumn = 'no',
+  },
+  filters = {
+    dotfiles = true,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  actions = {
+    expand_all = {
+      exclude = { '.git', 'build', 'dist', 'public' },
+    },
+  },
 }
 
-nnoremap {"<c-t>", "<cmd>:NvimTreeToggle<cr>"}
+require('nvim-tree').setup(setup)
