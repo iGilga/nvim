37cba79a6d28a4a21e300b76b8ffdf7574a42317
Gilga <pyua87@gmail.com>
2022-11-02 02:01:28 +0300

    Config colorizer only js developer.

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,33 +1,18 @@
-local setup = {
-  {
-    -- '*',
-    -- '!packer',
-    -- html = { mode = 'background' },
-  },
-  {
-    RGB = true, -- #RGB hex codes
-    RRGGBB = true, -- #RRGGBB hex codes
-    names = false, -- "Name" codes like Blue
-    RRGGBBAA = false, -- #RRGGBBAA hex codes
-    rgb_fn = false, -- CSS rgb() and rgba() functions
-    hsl_fn = false, -- CSS hsl() and hsla() functions
-    css = false, -- Enable all css features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-    mode = 'background', -- Set the display mode
-  },
-}
-
 require('colorizer').setup({
-  '*',
+  'html',
+  'css',
+  'javsscript',
+  'javascriptreact',
+  'lua',
   '!packer',
 }, {
-  RGB = true, -- #RGB hex codes
-  RRGGBB = true, -- #RRGGBB hex codes
-  names = false, -- "Name" codes like Blue
-  RRGGBBAA = false, -- #RRGGBBAA hex codes
-  rgb_fn = false, -- CSS rgb() and rgba() functions
-  hsl_fn = false, -- CSS hsl() and hsla() functions
-  css = false, -- Enable all css features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-  css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+  -- RGB = true, -- #RGB hex codes
+  -- RRGGBB = true, -- #RRGGBB hex codes
+  -- names = false, -- "Name" codes like Blue
+  -- RRGGBBAA = false, -- #RRGGBBAA hex codes
+  -- rgb_fn = false, -- CSS rgb() and rgba() functions
+  -- hsl_fn = false, -- CSS hsl() and hsla() functions
+  -- css = false, -- Enable all css features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+  -- css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
   mode = 'background', -- Set the display mode
 })
