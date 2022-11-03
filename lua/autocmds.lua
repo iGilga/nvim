2af033292b47b916b9fcd934b869961b3e068eea
Gilga <pyua87@gmail.com>
2022-11-03 04:31:55 +0300

    reorganized autocommands.

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,10 +1,13 @@
+local cmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+-- local createCommand = vim.api.nvim_create_user_command
 -- local logger = require('utils.logger')
 -- Auto sync plugins on save of plugins.lua
--- vim.api.nvim_create_autocmd("BufWritePost", { pattern = "plugins.lua", command = "source <afile> | PackerSync" })
+-- cmd("BufWritePost", { pattern = "plugins.lua", command = "source <afile> | PackerSync" })
 --
 -- Highlight on yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
+local highlight_group = augroup('YankHighlight', { clear = true })
+cmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank({ timeout = 500 })
   end,
@@ -13,33 +16,51 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Disable diagnostics in node_modules (0 is current buffer only)
--- vim.api.nvim_create_autocmd("BufRead", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
--- vim.api.nvim_create_autocmd("BufNewFile", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
+-- cmd("BufRead", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
+-- cmd("BufNewFile", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
 
 -- Enable spell checking for certain file types
--- vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, { pattern = { '*.md', '*.tex' }, command = 'setlocal spell' })
+-- cmd({ 'BufRead', 'BufNewFile' }, { pattern = { '*.md', '*.tex' }, command = 'setlocal spell' })
 
-vim.api.nvim_create_autocmd({ 'BufEnter' }, { command = ':lua require("lazygit.utils").project_root_dir()' })
+cmd({ 'BufEnter' }, { command = 'lua require("lazygit.utils").project_root_dir()' })
 
-vim.api.nvim_create_autocmd(
-  'BufRead',
-  { pattern = '*/openra/*', command = 'setlocal noexpandtab tabstop=4 shiftwidth=4' }
-)
+-- cmd({ 'BufEnter' }, { command = 'silent! setlocal foldlevel=99' })
 
-local wrap_md_group = vim.api.nvim_create_augroup('Markdown Wrap Settings', { clear = true })
+cmd('BufRead', { pattern = '*/openra/*', command = 'setlocal noexpandtab tabstop=4 shiftwidth=4' })
 
-vim.api.nvim_create_autocmd('BufEnter', {
+local wrap_md_group = augroup('Markdown Wrap Settings', { clear = true })
+
+cmd('BufEnter', {
   pattern = { '*.md' },
   group = wrap_md_group,
   command = 'setlocal wrap',
 })
 
-local customSessionGroup = vim.api.nvim_create_augroup('CustomSessionGroup', {})
+local customSessionGroup = augroup('CustomSessionGroup', {})
 
-vim.api.nvim_create_autocmd('User', {
+cmd('User', {
   pattern = 'SessionSavePost',
   group = customSessionGroup,
   callback = function()
     require('utils.logger').info('Session saved.')
+  end,
+})
+
+augroup('alpha_settings', { clear = true })
+cmd('User', {
+  group = 'alpha_settings',
+  pattern = 'AlphaReady',
+  callback = function()
+    local ls = vim.opt.laststatus
+    local st = vim.opt.showtabline
+    vim.opt.laststatus = 0
+    vim.opt.showtabline = 0
+    cmd('BufUnload', {
+      pattern = '<buffer>',
+      callback = function()
+        vim.opt.laststatus = ls
+        vim.opt.showtabline = st
+      end,
+    })
   end,
 })
