7f0087acdb1d6aad31fe64c21853400fb932161e
Gilga <pyua87@gmail.com>
2022-11-04 06:01:34 +0300

    Change size.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,8 +7,8 @@ local setup = {
     show_buffer_close_icons = false,
     show_tab_indicators = false,
     show_close_icon = false,
-    max_name_length = 14,
-    max_prefix_length = 8,
+    max_name_length = 20,
+    max_prefix_length = 10,
     tab_size = 16,
   },
 }
