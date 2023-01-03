f7b41adf1456270913d4042445278b12f34f452e
Gilga <pyua87@gmail.com>
2023-01-03 06:33:09 +0300

    Added global check config for lsp format.

--- lua/global.lua
+++ lua/global.lua
@@ -1,3 +1,5 @@
+local config = require('config')
+
 P = function(...)
   vim.pretty_print(...)
 end
@@ -6,4 +8,12 @@ _G.valhalla = {}
 
 function valhalla.merge(...)
   return vim.tbl_deep_extend('force', ...)
+end
+
+function valhalla.isClientFormat(clientName)
+  if config.lsp.servers[clientName] then
+    return (config.lsp.servers[clientName].format == true)
+  end
+
+  return false
 end
