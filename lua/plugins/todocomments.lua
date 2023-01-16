bd2a82757ee627873e3a4a1955dd20fa3407f0eb
Gilga <pyua87@gmail.com>
2023-01-16 10:52:10 +0300

    Update todo-comments

--- lua/plugins/todocomments.lua
+++ lua/plugins/todocomments.lua
@@ -1,30 +1,36 @@
 local setup = {
   keywords = {
-    TODO = {
-      alt = { 'TODO', 'todo' },
-    },
-    NOTE = {
-      alt = { 'NOTE', 'note' },
-    },
     FIX = {
-      alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE', 'fix', 'fixme', 'fixit' },
+      icon = ' ',
+      color = 'error',
+      alt = { 'fix', 'FIXME', 'BUG', 'fixme', 'bug' },
     },
-    WARN = {
-      alt = { 'WARN', 'XXX', 'warn', 'xxx' },
-    },
-    PERF = {
-      alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE', 'optim', 'performance', 'optimize' },
-    },
-    HACK = {
-      alt = { 'HACK', 'hack' },
-    },
+    TODO = { icon = ' ', color = 'info', alt = { 'todo' } },
+    HACK = { icon = ' ', color = 'warning', alt = { 'hack' } },
+    WARN = { icon = ' ', color = 'warning', alt = { 'warn' } },
+    PERF = { icon = ' ', color = 'perf', alt = { 'perf' } },
+    NOTE = { icon = ' ', color = 'hint', alt = { 'note', 'INFO', 'info' } },
+    TEST = { icon = 'ﭧ ', color = 'test', alt = { 'test' } },
   },
   colors = {
     error = { '#C34043' },
     warning = { '#DCA561' },
     info = { '#7E9CD8' },
-    hint = { '#76946A' },
+    test = { '#957fB8' },
+    perf = { '#D27E99' },
     default = { 'Identifier', '#957FB8' },
+  },
+  search = {
+    command = 'rg',
+    args = {
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--ignore-case',
+    },
+    pattern = [[\b(KEYWORDS):]],
   },
 }
 
@@ -32,11 +38,16 @@ local setup = {
 -- note: note
 -- fix: fix
 -- warn: warn
--- optim: optim
+-- perf: perf
 -- hack: hack
+-- test: test
 
-vim.keymap.set('n', '<leader>ft', ':TodoTelescope<cr>', { desc = "telescope with todo" })
-vim.keymap.set("n", "]t", function() require("todo-comments").jump_next() end, { desc = "next todo comment" })
-vim.keymap.set("n", "[t", function() require("todo-comments").jump_prev() end, { desc = "previous todo comment" })
+vim.keymap.set('n', '<leader>ft', ':TodoTelescope<cr>', { desc = 'telescope with todo' })
+vim.keymap.set('n', ']t', function()
+  require('todo-comments').jump_next()
+end, { desc = 'next todo comment' })
+vim.keymap.set('n', '[t', function()
+  require('todo-comments').jump_prev()
+end, { desc = 'previous todo comment' })
 
 require('todo-comments').setup(setup)
