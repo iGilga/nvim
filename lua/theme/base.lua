52c3d957b7ec7625862d91e13a2db535007f05fa
Gilga <pyua87@gmail.com>
2023-02-09 07:49:01 +0300

    Disable hl lsp definition. Instead of using plugin illimunate.

--- lua/theme/base.lua
+++ lua/theme/base.lua
@@ -2,6 +2,6 @@ return {
   NormalFloat = { fg = C.white, bg = C.dark3 },
   FloatBorder = { fg = C.gray, bg = C.dark3 },
   CBorder = { fg = C.green, bg = C.green },
-  TSDefinitionUsage = { fg = C.bg, bg = C.green },
-  TSDefinition = { fg = C.bg, bg = C.yellow },
+  -- TSDefinitionUsage = { fg = C.bg, bg = C.green },
+  -- TSDefinition = { fg = C.bg, bg = C.yellow },
 }
