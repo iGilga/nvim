94b14d7af89a7c915be35aa203c7ba9c3a217a43
Gilga <pyua87@gmail.com>
2023-02-23 01:07:55 +0300

    Change config for norg.

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -55,12 +55,25 @@ return {
     ft = 'norg',
     config = function()
       require('neorg').setup({
-        ['core.defaults'] = {},
-        ['core.norg.dirman'] = {
-          config = {
-            workspaces = {
-              work = '~/notes/work',
-              home = '~/notes/home',
+        load = {
+          ['core.defaults'] = {},
+          ['core.norg.completion'] = { config = { 'nvim-cmp' } },
+          ['core.norg.concealer'] = {
+            config = {
+              icon_preset = 'varied',
+              folds = false,
+              dim_code_blocks = {
+                enabled = true,
+                padding = { left = 1 },
+              },
+            },
+          },
+          ['core.norg.dirman'] = {
+            config = {
+              workspaces = {
+                work = '~/notes/work',
+                home = '~/notes/home',
+              },
             },
           },
         },
