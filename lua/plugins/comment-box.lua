bc11d213f053837a0381a7f520e2ab4b7708ad23
Gilga <pyua87@gmail.com>
2023-02-24 21:48:11 +0300

    Rekeybind for comment-box

--- lua/plugins/comment-box.lua
+++ lua/plugins/comment-box.lua
@@ -1,14 +1,19 @@
 local function setup()
-  local keymap = vim.api.nvim_set_keymap
-
-  keymap('n', '<Leader>cl', "<Cmd>lua require('comment-box').lbox()<CR>", {})
-  keymap('v', '<Leader>cl', "<Cmd>lua require('comment-box').lbox()<CR>", {})
-
-  keymap('n', '<Leader>cc', "<Cmd>lua require('comment-box').cbox()<CR>", {})
-  keymap('v', '<Leader>cc', "<Cmd>lua require('comment-box').cbox()<CR>", {})
-
-  keymap('n', '<Leader>ci', "<Cmd>lua require('comment-box').line()<CR>", {})
-  keymap('i', '<-l>', "<Cmd>lua require('comment-box').line()<CR>", {})
+  local map = vim.api.nvim_set_keymap
+  local opts = function(desc)
+    return { noremap = true, silent = true, desc = desc }
+  end
+  map('n', '<Leader>cl', "<Cmd>lua require('comment-box').lbox(2)<CR>", opts('box'))
+  map('v', '<Leader>cl', "<Cmd>lua require('comment-box').lbox(2)<CR>", opts('box'))
+  -- centered adapted box with centered text
+  map('n', '<Leader>ca', "<Cmd>lua require('comment-box').accbox(2)<CR>", opts('centred box'))
+  map('v', '<Leader>ca', "<Cmd>lua require('comment-box').accbox(2)<CR>", opts('centred box'))
+  -- full box
+  map('n', '<Leader>cc', "<Cmd>lua require('comment-box').cbox(2)<CR>", opts('full box'))
+  map('v', '<Leader>cc', "<Cmd>lua require('comment-box').cbox(2)<CR>", opts('full box'))
+  -- centered line
+  map('n', '<Leader>ci', "<Cmd>lua require('comment-box').cline(1)<CR>", opts('line'))
+  -- map('i', '<M-l>', "<Cmd>lua require('comment-box').cline(1)<CR>", opts('line'))
 end
 
 return {
