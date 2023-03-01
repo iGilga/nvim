00d37c82eee69b6026e0499f16cb1f41f3c190e4
Gilga <pyua87@gmail.com>
2023-03-01 05:13:46 +0300

    Fix, getting error after using hop.

--- lua/theme/plugins/hop.lua
+++ lua/theme/plugins/hop.lua
@@ -1,8 +1,8 @@
 return {
-  -- HopNextKey = { fg = C.roninYellow, bold = true },
-  -- HopNextKey1 = { fg = C.waveAqua1, bold = true },
-  -- HopNextKey2 = { fg = C.waveAqua2 },
-  -- HopUnmatched = { fg = C.fujiGray },
-  -- HopCursor = { link = 'Cursor' },
+  HopNextKey = { fg = C.roninYellow, bold = true },
+  HopNextKey1 = { fg = C.waveAqua1, bold = true },
+  HopNextKey2 = { fg = C.waveAqua2 },
+  HopUnmatched = { fg = C.fujiGray },
+  HopCursor = { link = 'Cursor' },
   -- HopPreview = { fg = C.surimiOrange, bold = true },
 }
