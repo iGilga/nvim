d68292571438d0f6c2e641f8cab4c74e2fd5da17
Gilga <pyua87@gmail.com>
2023-03-01 05:16:57 +0300

    Remove custom comment for js and jsx.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -30,23 +30,6 @@ local setup = {
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
-    config = {
-      javascript = {
-        __default = '// %s',
-        jsx_element = '{/* %s */}',
-        jsx_fragment = '{/* %s */}',
-        jsx_attribute = '// %s',
-        comment = '// %s',
-      },
-      javascriptreact = {
-        __default = '// %s',
-        jsx_element = '{/* %s */}',
-        jsx_fragment = '{/* %s */}',
-        jsx_attribute = '// %s',
-        comment = '// %s',
-      },
-      typescript = { __default = '// %s', __multiline = '/* %s */' },
-    },
   },
   refactor = {
     highlight_definitions = { enable = true },
