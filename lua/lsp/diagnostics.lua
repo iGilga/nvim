6b30ecfc62ec8f34fcb0f9734ff2ddfde9a7cb2d
iGilga <pyua87@gmail.com>
2023-05-26 02:49:21 +0300

    Fix vim.diagnostic

--- lua/lsp/diagnostics.lua
+++ lua/lsp/diagnostics.lua
@@ -1,10 +1,10 @@
-local config = require('config.user')
+-- local config = require('config.user')
 
 local signs = {
-  Error = ' ',
-  Warn = ' ',
-  Hint = ' ',
-  Info = ' ',
+  Error = '󰅚',
+  Warn = '󰀪',
+  Hint = '󰌶',
+  Info = '󰋽',
 }
 
 for type, icon in pairs(signs) do
@@ -12,21 +12,21 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
 end
 
--- vim.diagnostic.config = {
---   underline = true,
---   signs = true,
---   update_in_insert = false,
---   severity_sort = true,
---   float = {
---     border = config.border,
---     focusable = false,
---     source = 'always',
---   },
---   virtual_text = {
---     spacing = 4,
---     source = 'always',
---     severity = {
---       min = vim.diagnostic.severity.HINT,
---     },
---   },
--- }
+vim.diagnostic.config({
+  underline = true,
+  signs = true,
+  -- float = {
+  -- border = config.border,
+  -- focusable = false,
+  -- source = 'always',
+  -- },
+  virtual_text = {
+    spacing = 1,
+    -- source = 'always',
+    prefix = '󰝤',
+    -- format = function(diagnostic)
+    -- P(diagnostic)
+    -- return string.format('[%s]%s', diagnostic.source, diagnostic.message)
+    -- end,
+  },
+})
