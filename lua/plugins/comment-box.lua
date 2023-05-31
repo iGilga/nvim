b6a3501ece4b9d789df71f57a38750bf85dd4bae
iGilga <pyua87@gmail.com>
2023-05-31 18:20:45 +0300

    Renamed to new version.

--- lua/plugins/comment-box.lua
+++ lua/plugins/comment-box.lua
@@ -1,9 +1,9 @@
 return {
   'LudoPinelli/comment-box.nvim',
   keys = {
-    { '<leader>cl', "<cmd>lua require('comment-box').lbox(2)<cr>", mode = { 'n', 'v' }, desc = 'comment box' },
-    { '<leader>ca', "<cmd>lua require('comment-box').accbox(2)<cr>", mode = { 'n', 'v' }, desc = 'centred box' },
-    { '<leader>cc', "<cmd>lua require('comment-box').cbox(2)<cr>", mode = { 'n', 'v' }, desc = 'full box' },
-    { '<leader>ci', "<cmd>lua require('comment-box').cline(1)<cr>", mode = { 'n', 'v' }, desc = 'line' }
+    { '<leader>cl', "<cmd>lua require('comment-box').llbox(2)<cr>", mode = { 'n', 'v' }, desc = 'Left box of fixed size with Left text' },
+    { '<leader>ca', "<cmd>lua require('comment-box').acbox(2)<cr>", mode = { 'n', 'v' }, desc = 'Centered adapted box' },
+    { '<leader>cc', "<cmd>lua require('comment-box').ccbox(2)<cr>", mode = { 'n', 'v' }, desc = 'Centered box of fixed size with Centered text' },
+    { '<leader>ci', "<cmd>lua require('comment-box').cline(1)<cr>", mode = { 'n', 'v' }, desc = 'Centered line' }
   },
 }
