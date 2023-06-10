adf1e333686a61de69cb1da1e104c618995283f5
iGilga <pyua87@gmail.com>
2023-06-10 03:35:30 +0300

    Added command toogle diagnostic virtual text.

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,3 +1,13 @@
 vim.api.nvim_create_user_command('Format', function()
   vim.lsp.buf.format({ async = true })
 end, { desc = 'Format file with LSP' })
+
+vim.api.nvim_create_user_command('DiagnosticToggle', function ()
+  local config = vim.diagnostic.config
+  local vt = config().virtual_text
+  config {
+    virtual_text = not vt,
+    underline = not vt,
+    -- signs = not vt,
+  }
+end, { desc = 'toggle diagnostic' })
