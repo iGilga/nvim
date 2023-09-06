698dd77b9269e6914b22ced91f503e12c7d0e7ec
iGilga <pyua87@gmail.com>
2023-09-06 15:07:05 +0300

    Added tailwind sorter.

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -1,5 +1,16 @@
 return {
-  'laytan/tailwind-sorter.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
-  build = 'cd formatter && npm i && npm run build',
+  {
+    'laytan/tailwind-sorter.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
+    build = 'cd formatter && npm i && npm run build',
+  },
+  {
+    'roobert/tailwindcss-colorizer-cmp.nvim',
+    -- optionally, override the default options:
+    config = function()
+      require('tailwindcss-colorizer-cmp').setup({
+        color_square_width = 2,
+      })
+    end,
+  },
 }
