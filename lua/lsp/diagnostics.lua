87c48e3fdebece1e9dd93bfd9d8999bc497b977f
iGilga <pyua87@gmail.com>
2024-01-03 11:19:24 +0300

    Check version neovim and enable new feature in 10.

--- lua/lsp/diagnostics.lua
+++ lua/lsp/diagnostics.lua
@@ -7,16 +7,26 @@ local signs = {
   Info = '󰋽',
 }
 
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+if vim.fn.has("nvim-0.9.0") == 1 then
+  print('DiagnosticSign')
+  for type, icon in pairs(signs) do
+    local hl = 'DiagnosticSign' .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+  end
 end
 
 vim.diagnostic.config({
   update_in_instert = true,
   severity_sort = true,
   underline = true,
-  signs = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = signs.Error,
+      [vim.diagnostic.severity.WARN] = signs.Warn,
+      [vim.diagnostic.severity.HINT] = signs.Hint,
+      [vim.diagnostic.severity.INFO] = signs.Info,
+    }
+  },
   float = {
     border = config.border,
     -- focusable = false,
