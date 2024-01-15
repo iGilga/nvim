d0659bae2df43b59c45b0e3da74788d621b43f42
iGilga <pyua87@gmail.com>
2024-01-15 23:47:13 +0300

    Added command for toogle this plugin.

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -28,72 +28,72 @@ local function setup()
   npairs.add_rules({
     npairs.add_rules({
       Rule(' ', ' ', { '-markdown' })
-        :with_pair(function(opts)
-          local pair = opts.line:sub(opts.col - 1, opts.col)
-          return vim.tbl_contains({ '()', '{}', '[]' }, pair)
-        end)
-        :with_move(cond.none())
-        :with_cr(cond.none())
-        :with_del(function(opts)
-          local col = vim.api.nvim_win_get_cursor(0)[2]
-          local context = opts.line:sub(col - 1, col + 2)
-          return vim.tbl_contains({ '(  )', '{  }', '[  ]' }, context)
-        end),
+          :with_pair(function(opts)
+            local pair = opts.line:sub(opts.col - 1, opts.col)
+            return vim.tbl_contains({ '()', '{}', '[]' }, pair)
+          end)
+          :with_move(cond.none())
+          :with_cr(cond.none())
+          :with_del(function(opts)
+            local col = vim.api.nvim_win_get_cursor(0)[2]
+            local context = opts.line:sub(col - 1, col + 2)
+            return vim.tbl_contains({ '(  )', '{  }', '[  ]' }, context)
+          end),
       Rule('', ' )')
-        :with_pair(cond.none())
-        :with_move(function(opts)
-          return opts.char == ')'
-        end)
-        :with_cr(cond.none())
-        :with_del(cond.none())
-        :use_key(')'),
+          :with_pair(cond.none())
+          :with_move(function(opts)
+            return opts.char == ')'
+          end)
+          :with_cr(cond.none())
+          :with_del(cond.none())
+          :use_key(')'),
       Rule('', ' }')
-        :with_pair(cond.none())
-        :with_move(function(opts)
-          return opts.char == '}'
-        end)
-        :with_cr(cond.none())
-        :with_del(cond.none())
-        :use_key('}'),
+          :with_pair(cond.none())
+          :with_move(function(opts)
+            return opts.char == '}'
+          end)
+          :with_cr(cond.none())
+          :with_del(cond.none())
+          :use_key('}'),
       Rule('', ' ]')
-        :with_pair(cond.none())
-        :with_move(function(opts)
-          return opts.char == ']'
-        end)
-        :with_cr(cond.none())
-        :with_del(cond.none())
-        :use_key(']'),
+          :with_pair(cond.none())
+          :with_move(function(opts)
+            return opts.char == ']'
+          end)
+          :with_cr(cond.none())
+          :with_del(cond.none())
+          :use_key(']'),
     }),
     Rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' })
-      :use_regex(true)
-      :set_end_pair_length(2),
+        :use_regex(true)
+        :set_end_pair_length(2),
     Rule('=', '', { '-vim', '-sh', '-cfg', '-conf', '-html' })
-      :with_pair(cond.not_inside_quote())
-      :with_pair(function(opts)
-        local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
-        if last_char:match('[%w%=%s]') then
-          return true
-        end
-        return false
-      end)
-      :replace_endpair(function(opts)
-        local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
-        local next_char = opts.line:sub(opts.col, opts.col)
-        next_char = next_char == ' ' and '' or ' '
-        if prev_2char:match('%w$') then
-          return '<bs> =' .. next_char
-        end
-        if prev_2char:match('%=$') then
-          return next_char
-        end
-        if prev_2char:match('=') then
-          return '<bs><bs>=' .. next_char
-        end
-        return ''
-      end)
-      :set_end_pair_length(0)
-      :with_move(cond.none())
-      :with_del(cond.none()),
+        :with_pair(cond.not_inside_quote())
+        :with_pair(function(opts)
+          local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
+          if last_char:match('[%w%=%s]') then
+            return true
+          end
+          return false
+        end)
+        :replace_endpair(function(opts)
+          local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
+          local next_char = opts.line:sub(opts.col, opts.col)
+          next_char = next_char == ' ' and '' or ' '
+          if prev_2char:match('%w$') then
+            return '<bs> =' .. next_char
+          end
+          if prev_2char:match('%=$') then
+            return next_char
+          end
+          if prev_2char:match('=') then
+            return '<bs><bs>=' .. next_char
+          end
+          return ''
+        end)
+        :set_end_pair_length(0)
+        :with_move(cond.none())
+        :with_del(cond.none()),
   })
 
   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
@@ -115,6 +115,17 @@ local function setup()
       },
     })
   )
+
+  local isPairs = true;
+  vim.api.nvim_create_user_command('AutopairsToggle', function()
+    if isPairs then
+      npairs.disable()
+      isPairs = false;
+    elseif not isPairs then
+      npairs.enable()
+      isPairs = true;
+    end
+  end, {})
 end
 
 return {
