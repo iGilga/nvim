4bddebd2f9df9a3ac0acd232ed85af2464db4c4e
iGilga <pyua87@gmail.com>
2024-01-30 10:04:04 +0300

    Rework for rename.

--- lua/utils/rename.lua
+++ lua/utils/rename.lua
@@ -18,10 +18,6 @@ local config = {
   highlight = 'Normal:NuiNormal',
 }
 
--- local config = require('config.user').rename
-local u = require('utils')
-local logger = require('utils.logger').Logger
-local title = '[LSP]Rename'
 local M = {}
 
 local minWidth = config.min_width or 30
@@ -34,7 +30,6 @@ local prompt_backspace = function(prompt)
   local cursor = vim.api.nvim_win_get_cursor(0)
   local line = cursor[1]
   local col = cursor[2]
-
   if col ~= prompt then
     vim.api.nvim_buf_set_text(0, line - 1, col - 1, line - 1, col, { '' })
     vim.api.nvim_win_set_cursor(0, { line, col - 1 })
@@ -51,36 +46,17 @@ local mappings = function(input)
   map(input, '<C-c>', function()
     input.input_props.on_close()
   end)
-
   map(input, '<BS>', function()
     prompt_backspace(prompt)
   end)
 end
 
-local renameCallback = function(err, result, ctx, _)
-  if err then
-    logger.error(("Error running LSP query '%s': %s"):format(ctx.method, err), title)
-  end
-
-  if result and result.changes then
-    local p = ctx.params
-    local uri = p.textDocument.uri
-    local client = lsp.get_client_by_id(ctx.client_id)
-    lsp.util.apply_workspace_edit(result, client.offset_encoding)
-    local count = #result.changes[uri]
-    local msg = ('%s -> %s [%s][%d]'):format(p.curName, p.newName, u.getRelativePath(uri), count)
-    logger.info(msg, title)
-  end
-end
-
 M.rename = function(popup_opts, opts)
   local curName = vim.fn.expand('<cword>')
-
   local width = minWidth
   if #curName > width then
     width = #curName
   end
-
   popup_opts = {
     position = {
       row = 1,
@@ -90,7 +66,7 @@ M.rename = function(popup_opts, opts)
       width = width,
     },
     relative = 'cursor',
-    highlight = config.highlight or 'Normal:Normal',
+    highlight = config.highlight,
     border = {
       highlight = config.border.highlight,
       style = config.border.style,
@@ -98,7 +74,6 @@ M.rename = function(popup_opts, opts)
       padding = { -1, -1 },
     },
   }
-
   opts = {
     prompt = Text(config.prompt, config.prompt_hl),
     default_value = curName,
@@ -106,20 +81,13 @@ M.rename = function(popup_opts, opts)
       if not (newName and #newName > 0) or newName == curName then
         return
       end
-      local params = lsp.util.make_position_params()
-      params.curName = curName
-      params.newName = newName
-      lsp.buf_request(0, 'textDocument/rename', params, renameCallback)
+      lsp.buf.rename(newName)
     end,
   }
-
   local input = Input(popup_opts, opts)
-
   -- mount/open the component
   input:mount()
-
   mappings(input)
-
   -- unmount component when cursor leaves buffer
   input:on(event.BufLeave, function()
     input:unmount()
