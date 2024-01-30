25c9060c2399272978f80f3d921ee0374847ab75
iGilga <pyua87@gmail.com>
2024-01-30 11:34:56 +0300

    added event.

--- lua/plugins/tabout.lua
+++ lua/plugins/tabout.lua
@@ -1,12 +1,12 @@
 local setup = {
-  tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
+  tabkey = '<Tab>',             -- key to trigger tabout, set to an empty string to disable
   backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
-  act_as_tab = true, -- shift content if tab out is not possible
-  act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-  default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-  default_shift_tab = '<C-d>', -- reverse shift default action,
-  enable_backwards = true, -- well ...
-  completion = true, -- if the tabkey is used in a completion pum
+  act_as_tab = true,            -- shift content if tab out is not possible
+  act_as_shift_tab = false,     -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+  default_tab = '<C-t>',        -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+  default_shift_tab = '<C-d>',  -- reverse shift default action,
+  enable_backwards = true,      -- well ...
+  completion = true,            -- if the tabkey is used in a completion pum
   tabouts = {
     { open = "'", close = "'" },
     { open = '"', close = '"' },
@@ -21,6 +21,7 @@ local setup = {
 
 return {
   'abecodes/tabout.nvim',
+  event = 'InsertEnter',
   dependencies = {
     'nvim-treesitter',
     'nvim-cmp',
