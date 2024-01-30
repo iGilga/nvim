0fdfc7bd79f08f3f7009f444855d3cc83c722f95
iGilga <pyua87@gmail.com>
2024-01-30 11:35:26 +0300

    reformatting.

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -6,23 +6,11 @@ return {
     local tb = require('trouble')
     return {
       { '<leader>xx', tb.toggle },
-      {
-        "<leader>xd",
-        function()
-          tb.toggle('document_diagnostics')
-        end,
-        desc = "Document Diagnostics (Trouble)"
-      },
-      {
-        '<leader>xw',
-        function()
-          tb.toggle('workspace_diagnostics')
-        end,
-        desc = "Workspace Diagnostics (Trouble)"
-      },
-      { "<leader>xl", function() tb.toggle('loclist') end,        desc = "Location List (Trouble)" },
-      { "<leader>xq", function() tb.toggle('quickfix') end,       desc = "Quickfix List (Trouble)" },
-      { "<leader>xr", function() tb.toggle('lsp_references') end, desc = "Refetence List (Trouble)" },
+      { "<leader>xd", function() tb.toggle('document_diagnostics') end,  desc = "Document Diagnostics (Trouble)" },
+      { '<leader>xw', function() tb.toggle('workspace_diagnostics') end, desc = "Workspace Diagnostics (Trouble)" },
+      { "<leader>xl", function() tb.toggle('loclist') end,               desc = "Location List (Trouble)" },
+      { "<leader>xq", function() tb.toggle('quickfix') end,              desc = "Quickfix List (Trouble)" },
+      { "<leader>xr", function() tb.toggle('lsp_references') end,        desc = "Refetence List (Trouble)" },
       {
         "[q",
         function()
