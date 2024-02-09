432221eadb1784d7151b78edc2fd5cbc0a8438be
iGilga <pyua87@gmail.com>
2024-02-09 11:58:55 +0300

    Enable tab.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -51,10 +51,10 @@ local function setup()
         if cmp.visible() then
           cmp.select_next_item()
         elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-          -- vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
-        elseif has_words_before() then
-          cmp.complete()
+          -- luasnip.expand_or_jump()
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+          -- elseif has_words_before() then
+          -- cmp.complete()
         else
           fallback()
         end
@@ -63,8 +63,8 @@ local function setup()
         if cmp.visible() then
           cmp.select_prev_item()
         elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-          -- vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+          -- luasnip.jump(-1)
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
         else
           fallback()
         end
