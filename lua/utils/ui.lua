d4c928e0c272b2a75bb1b3282a1c92deeb146918
iGilga <pyua87@gmail.com>
2024-02-20 01:28:06 +0300

    new ui using nui for vim.ui.select.

--- lua/utils/ui.lua
+++ lua/utils/ui.lua
@@ -7,29 +7,103 @@ local event = require('nui.utils.autocmd').event
 
 local config = {
   min_width = 80,
+  text = {
+    hl_gray = 'NuiBottom'
+  },
   border = {
-    bottom_hl = 'NuiBottom',
-    highlight = 'NuiBorder',
     style = 'solid',
   },
   title = {
-    title_align = 'center',
-    highlight = 'NuiTitle',
+    align = 'center',
+    hl = 'NuiTitle',
   },
   separator = {
     char = ' ',
-    text_align = 'center',
-    highlight = 'NuiSeparator',
+    align = 'center',
+    hl = 'NuiSeparator',
   },
-  highlight = 'Normal:NuiNormal',
 }
 
 local function get_prompt_text(prompt, default_prompt)
   local prompt_text = prompt or default_prompt
   if prompt_text:sub(-1) == ":" then
-    prompt_text = "[" .. prompt_text:sub(1, -2) .. "]"
+    prompt_text = prompt_text:sub(1, -2)
   end
   return prompt_text
+end
+
+local function build_popup_opts(title)
+  local popup_options = {
+    relative = 'editor',
+    position = '50%',
+    border = {
+      style = config.border.style,
+      text = {
+        top = ntext(' ' .. title .. ' ', config.title.hl),
+        top_align = config.title.align,
+      }
+    },
+    win_options = {
+      winhighlight = 'NormalFloat:NuiNormal,FloatBorder:NuiBorder',
+    },
+    zindex = 999,
+  }
+  return popup_options
+end
+
+local function build_lines(items, format_item)
+  local menu_items = {}
+  for index, item in ipairs(items) do
+    if type(item) ~= 'table' then
+      item = { __raw_item = item }
+    end
+    item.index = index
+    menu_items[index] = nmenu.item(format_item(item), item)
+  end
+  return menu_items
+end
+
+local function build_format_item(opts, title)
+  local format_item = nil
+  local case = {
+    ['Load Session'] = function(item)
+      local path = item.dir.filename
+      local dir = ntext((vim.fs.dirname(path):gsub('/home/stag', '~')) .. '/', config.text.hl_gray)
+      local basename = ntext(vim.fs.basename(path), 'NuiText')
+      return nline({ dir, basename })
+    end
+  }
+  if case[title] then
+    format_item = case[title]
+  else
+    format_item = opts.format_item or function(item)
+      return tostring(item.__raw_item or item)
+    end
+  end
+  return format_item
+end
+
+local function build_menu_opts(title, lines, on_done)
+  return {
+    min_width = vim.api.nvim_strwidth(title) + 40,
+    lines = lines,
+    on_close = function()
+      on_done(nil, nil)
+    end,
+    on_submit = function(item)
+      on_done(item.__raw_item or item, item.index)
+    end,
+  }
+end
+
+local function buildOptions(items, opts, on_done)
+  local title = get_prompt_text(opts.prompt, "Select item")
+  local format_item = build_format_item(opts, title)
+  local menu_items = build_lines(items, format_item)
+
+  local popup_opts = build_popup_opts(title)
+  local menu_opts = build_menu_opts(title, menu_items, on_done)
+  return popup_opts, menu_opts
 end
 
 local function override_ui_select()
@@ -37,73 +111,8 @@ local function override_ui_select()
   local UISelect = nmenu:extend("UISelect")
 
   function UISelect:init(items, opts, on_done)
-    local border_top_text = get_prompt_text(opts.prompt, "[Select item]")
-    local kind = opts.kind or 'unknown'
-    local format_item = nil
-    if opts.prompt == 'Load Session' then
-      format_item = function(item)
-        local path = item.dir.filename
-        local dir = ntext((vim.fs.dirname(path):gsub('/home/stag', '~')) .. '/', config.border.bottom_hl)
-        local basename = ntext(vim.fs.basename(path), 'NuiText')
-        return nline({ dir, basename })
-      end
-    else
-      format_item = opts.format_item
-    end
-    -- local format_item = opts.format_item or function(item)
-    --   return tostring(item.__raw_item or item)
-    -- end
-    local popup_options = {
-      relative = 'editor',
-      position = '50%',
-      highlight = config.highlight,
-      border = {
-        highlight = config.border.highlight,
-        style = config.border.style,
-        text = {
-          top = ntext(' ' .. border_top_text .. ' ', config.title.highlight),
-          top_align = config.title.title_align,
-        }
-      },
-      -- win_options = {
-      -- winhighlight = 'NormalFloat:Normal,FloatBorder:Normal',
-      -- },
-      zindex = 999,
-    }
+    local popup_options, menu_options = buildOptions(items, opts, on_done)
 
-    if kind == 'codeaction' then
-      popup_options.relative = 'cursor'
-      popup_options.position = {
-        row = 1,
-        col = 0,
-      }
-    end
-
-    local max_widht = popup_options.relative == 'editor' and vim.o.columns - 4 or vim.api.nvim_win_get_width(0) - 4
-    local max_height = popup_options.relative == 'editor' and math.floor(vim.o.lines * 80 / 100) or
-        vim.api.nvim_win_get_height(0)
-
-    local menu_items = {}
-    for index, item in ipairs(items) do
-      if type(item) ~= 'table' then
-        item = { __raw_item = item }
-      end
-      item.index = index
-      menu_items[index] = nmenu.item(format_item(item), item)
-    end
-
-    local menu_options = {
-      min_width = vim.api.nvim_strwidth(border_top_text) + 40,
-      max_widht = max_widht,
-      max_height = max_height,
-      lines = menu_items,
-      on_close = function()
-        on_done(nil, nil)
-      end,
-      on_submit = function(item)
-        on_done(item.__raw_item or item, item.index)
-      end,
-    }
     UISelect.super.init(self, popup_options, menu_options)
     self:on(event.BufLeave, function()
       on_done(nil, nil)
