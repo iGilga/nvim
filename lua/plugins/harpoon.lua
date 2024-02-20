f102fae93bde49600cd7c187f1ba1896b13b2616
iGilga <pyua87@gmail.com>
2024-02-21 01:38:21 +0300

    added keys harpoon.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,5 +1,16 @@
 return {
   'ThePrimeagen/harpoon',
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = function()
+    local harpoon_ui = require("harpoon.ui")
+    local harpoon_mark = require("harpoon.mark")
+    return {
+      { "si", harpoon_ui.toggle_quick_menu, desc = "[harpoon] toggle quick menu" },
+      { "sm", harpoon_mark.add_file,        desc = "[harpoon] add file mark" },
+      { "sk", harpoon_ui.nav_next,          desc = "[harpoon] next" },
+      { "sj", harpoon_ui.nav_prev,          desc = "[harpoon] prev" },
+    }
+  end,
   config = function()
     require('harpoon').setup()
   end,
