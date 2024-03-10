50b6436edb0d583350e54703543e76a0381d6eee
iGilga <pyua87@gmail.com>
2024-03-10 19:35:16 +0300

    rework kaymap on togglable oil. All border style to solid.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,14 +1,42 @@
+local border_style = 'solid'
 local keys = function()
   local oil = require('oil')
   return
   {
-    { '<leader>m',  oil.open,  desc = 'open oil', },
-    { '<leader>mc', oil.close, desc = 'close oil', },
+    {
+      '<leader>m',
+      function()
+        if vim.bo.filetype == 'oil' then
+          oil.close()
+        else
+          oil.open()
+        end
+      end,
+      desc = 'Toogle oil(File navigation)',
+    },
+
   }
 end
 return {
   'stevearc/oil.nvim',
   keys = keys,
-  opts = {},
+  opts = {
+    skip_confirm_for_simple_edits = true,
+    keymaps_help = {
+      border = border_style,
+    },
+    float = {
+      border = border_style,
+    },
+    preview = {
+      border = border_style,
+    },
+    progress = {
+      border = border_style,
+    },
+    ssh = {
+      border = border_style
+    },
+  },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
 }
