2f24a8e61fcb35ab34faf9d85a7c693bf5d7236d
iGilga <pyua87@gmail.com>
2024-03-10 19:37:57 +0300

    Remove ranger.nvim

--- lua/plugins/ranger.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'kelly-lin/ranger.nvim',
-  keys = {
-    { '<c-t>', "<cmd>lua require('ranger-nvim').open(true)<cr>" },
-  },
-  config = function()
-    require('ranger-nvim').setup({ replace_netrw = true })
-  end,
-}
