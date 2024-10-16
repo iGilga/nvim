1ecfcd1fc9364191a6057ceb1342843ec96dad30
iGilga <pyua87@gmail.com>
2024-10-16 05:47:05 +0300

    rename tsserver to ts_ls.

--- lua/lsp/servers/ts_ls.lua
+++ lua/lsp/servers/ts_ls.lua
@@ -45,7 +45,6 @@ end
 local function on_attach(client, bufnr)
   client.server_capabilities.documentFormattingProvider = false
   client.server_capabilities.documentRangeFormattingProvider = false
-  defaultOnAttach(client, bufnr)
 end
 
 local handlers = {
