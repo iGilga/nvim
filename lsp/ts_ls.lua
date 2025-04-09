99050faab8dfbc3a30a92cd8789db56d2cfe51d1
iGilga <pyua87@gmail.com>
2025-04-09 11:19:11 +0300

    lsp for ts_ls

---
+++ lsp/ts_ls.lua
@@ -0,0 +1,79 @@
+local on_attach = require('lsp.mapping')
+-- local capabilities = {}
+--
+-- local ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+-- if ok then
+--   capabilities = cmp_nvim_lsp.default_capabilities()
+--
+--   capabilities.textDocument.completion.completionItem.snippetSupport = true
+--   capabilities.textDocument.completion.completionItem.preselectSupport = true
+--   capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+--   capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+--   capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+--   capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+--   capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+--   capabilities.textDocument.completion.completionItem.resolveSupport = {
+--     properties = {
+--       'documentation',
+--       'detail',
+--       'additionalTextEdits',
+--     },
+--   }
+--
+--   capabilities.textDocument.codeAction = {
+--     dynamicRegistration = false,
+--     codeActionLiteralSupport = {
+--       codeActionKind = {
+--         valueSet = {
+--           '',
+--           'quickfix',
+--           'refactor',
+--           'refactor.extract',
+--           'refactor.inline',
+--           'refactor.rewrite',
+--           'source',
+--           'source.organizeImports',
+--         },
+--       },
+--     },
+--   }
+-- end
+return {
+  init_options = { hostInfo = 'neovim' },
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx' },
+  -- root_dir = util.root_pattern('tsconfig.json', 'jsconfig.json', 'package.json', '.git'),
+  single_file_support = true,
+  settings = {
+    tsserver = {
+      typescript = {
+        inlayHints = {
+          includeInlayParameterNameHints = 'all',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = false,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = false,
+          includeInlayEnumMemberValueHints = true,
+        },
+      },
+      javascript = {
+        diagnostics = false,
+        inlayHints = {
+          includeInlayParameterNameHints = 'all',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = false,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = false,
+          includeInlayEnumMemberValueHints = true,
+        },
+      },
+    },
+  },
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+  end,
+}
