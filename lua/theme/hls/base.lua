ea34c27ac6417bad5e2695183b8ca709d12ce34f
iGilga <pyua87@gmail.com>
2025-04-11 06:47:19 +0300

    change color.

--- lua/theme/hls/base.lua
+++ lua/theme/hls/base.lua
@@ -7,6 +7,7 @@ function M.hl(colors)
     PmenuSel = { bg = colors.palette.sumiInk5 },
     PmenuThumb = { bg = colors.palette.sumiInk5 },
     PmenuSbar = { bg = colors.palette.sumiInk4 },
+    PmenuExtra = { bg = colors.palette.sumiInk1, fg = colors.fujiGray },
     ['@string.special.url'] = { undercurl = false, underline = true },
   }
 end
