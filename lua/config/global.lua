777cb4a5cd802b382223a776e1d203a2f0728ccf
iGilga <pyua87@gmail.com>
2025-04-14 21:10:27 +0300

    Added function logging to file.

--- lua/config/global.lua
+++ lua/config/global.lua
@@ -2,6 +2,21 @@ P = function(...)
   vim.print(...)
 end
 
+FP = function(s, target, flag)
+  local path = '/home/stag/logs/' .. s .. '.json'
+  local f = flag or false
+  if flag then
+    f = io.open(path, 'a+')
+  else
+    f = io.open(path, 'w')
+  end
+  if f then
+    local log = vim.json.encode(target)
+    f:write(log)
+    f:close()
+  end
+end
+
 _G.valhalla = {}
 
 function valhalla.merge(...)
